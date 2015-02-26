7c7581318a63b4c3e8fe56347218fd8cac08a662
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2015-02-26 23:07:17 +1100

    added example api_user factory

--- spec/factories/mno_enterprise_users.rb
+++ spec/factories/mno_enterprise_users.rb
@@ -1,6 +1,13 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
+# This is an API resource factory generating a Hash to be used in API stubs
+# Use as such: build(:api_user)
+# See http://stackoverflow.com/questions/10032760/how-to-define-an-array-hash-in-factory-girl
 FactoryGirl.define do
-  factory :mno_enterprise_user, :class => 'User' do
+  factory :api_user, class: Hash do
+    first_name "John"
+    last_name "Doe"
+    
+    initialize_with { attributes }
   end
 end
