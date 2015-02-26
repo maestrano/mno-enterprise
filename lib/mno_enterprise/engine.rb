6b77c5165a5d48b2c7aa3d615d308aa2df48d62e
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2015-02-26 19:15:18 +1100

    added factorygirl - changed default generator to rspec

--- lib/mno_enterprise/engine.rb
+++ lib/mno_enterprise/engine.rb
@@ -1,5 +1,10 @@
 module MnoEnterprise
   class Engine < ::Rails::Engine
     isolate_namespace MnoEnterprise
+    
+    config.generators do |g|
+      g.test_framework :rspec
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+    end
   end
 end
