31f4e2de689fe327c7ad8f9d3ee5861c7b8822b4
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2015-03-29 14:43:25 +1100

    disabled advanced features

--- app/controllers/mno_enterprise/jpi/v1/organization_controller.rb
+++ app/controllers/mno_enterprise/jpi/v1/organization_controller.rb
@@ -2,12 +2,12 @@ module MnoEnterprise
   class Jpi::V1::OrganizationController < Jpi::V1::BaseResourceController
     respond_to :json
 
-    # GET /jpi/v1/dashboard/:id/organization
+    # GET /jpi/v1/organization/:id
     def index
       render partial: 'index'
     end
 
-    # PUT /jpi/v1/dashboard/:id/organization
+    # PUT /jpi/v1/organization/:id
     def update
       # Authorize and update
       authorize! :update, @organization
@@ -26,7 +26,7 @@ module MnoEnterprise
       end
     end
 
-    # PUT /jpi/v1/dashboard/:id/organization/charge
+    # PUT /jpi/v1/organization/:id/charge
     def charge
       authorize! :manage_billing, @organization
       payment = @organization.charge
@@ -44,7 +44,7 @@ module MnoEnterprise
       render json: { status: s, data: payment }
     end
 
-    # PUT /jpi/v1/dashboard/:id/organization/update_billing
+    # PUT /jpi/v1/organization/:id/update_billing
     def update_billing
       whitelist = ['title','first_name','last_name','number','month','year','country','verification_value','billing_address','billing_city','billing_postcode', 'billing_country']
       attributes = params[:credit_card].select { |k,v| whitelist.include?(k.to_s) }
@@ -64,7 +64,7 @@ module MnoEnterprise
       end
     end
 
-    # PUT /jpi/v1/dashboard/:id/organization/invite_members
+    # PUT /jpi/v1/organization/:id/invite_members
     def invite_members
       # Filter
       whitelist = ['email','role','team_id']
@@ -90,7 +90,7 @@ module MnoEnterprise
       render partial: 'members'
     end
 
-    # PUT /jpi/v1/dashboard/:id/organization/update_member
+    # PUT /jpi/v1/organization/:id/update_member
     def update_member
       attributes = params[:member]
       @orga_relation = @organization.orga_relations.joins(:user).where("users.email = ?",attributes[:email]).first
@@ -109,7 +109,7 @@ module MnoEnterprise
       render partial: 'members'
     end
 
-    # PUT /jpi/v1/dashboard/:id/organization/remove_member
+    # PUT /jpi/v1/organization/:id/remove_member
     def remove_member
       attributes = params[:member]
       @member = User.find_by_email(attributes[:email])
@@ -126,50 +126,50 @@ module MnoEnterprise
       render partial: 'members'
     end
 
-    # PUT /jpi/v1/dashboard/:id/organization/update_support_plan
-    def update_support_plan
-      attributes = params['support_plan']
-      authorize! :update, @organization
-      if @organization.update_support_plan(attributes)
-        render partial: 'organization'
-      else
-        render json: @organization.errors, status: :bad_request
-      end
-    end
+    # PUT /jpi/v1/organization/:id/update_support_plan
+    # def update_support_plan
+    #   attributes = params['support_plan']
+    #   authorize! :update, @organization
+    #   if @organization.update_support_plan(attributes)
+    #     render partial: 'organization'
+    #   else
+    #     render json: @organization.errors, status: :bad_request
+    #   end
+    # end
 
     # PUT /jpi/v1/
-    def update_meta_data
-      # Authorize and update
-      authorize! :update, @organization
-
-      name = params[:name]
-      value = params[:value]
-
-      if name && value
-        render json: @organization.put_meta_data(name,value)
-      else
-        render json: '', status: :bad_request
-      end
-    end
+    # def update_meta_data
+    #   # Authorize and update
+    #   authorize! :update, @organization
+    #
+    #   name = params[:name]
+    #   value = params[:value]
+    #
+    #   if name && value
+    #     render json: @organization.put_meta_data(name,value)
+    #   else
+    #     render json: '', status: :bad_request
+    #   end
+    # end
 
 
     # POST /jpi/v1/dashboard/:id/organization/training_session_req
-    def training_session_req
-      if params['message'] && @organization.support_plan && @organization.support_plan.custom_training_credits > 0
-        # Consume a custom training credit
-        @organization.support_plan.consume_custom_training
-        attributes = {
-          'message' => params['message'],
-          'first_name' => current_user.name,
-          'last_name' => current_user.surname,
-          'email' => current_user.email,
-        }
-        PartnerMailer.delay.contact_partner(attributes)
-        render partial: 'organization'
-      else
-        render json: 'no message', status: :bad_request
-      end
-    end
+    # def training_session_req
+    #   if params['message'] && @organization.support_plan && @organization.support_plan.custom_training_credits > 0
+    #     # Consume a custom training credit
+    #     @organization.support_plan.consume_custom_training
+    #     attributes = {
+    #       'message' => params['message'],
+    #       'first_name' => current_user.name,
+    #       'last_name' => current_user.surname,
+    #       'email' => current_user.email,
+    #     }
+    #     PartnerMailer.delay.contact_partner(attributes)
+    #     render partial: 'organization'
+    #   else
+    #     render json: 'no message', status: :bad_request
+    #   end
+    # end
 
   end
 end
