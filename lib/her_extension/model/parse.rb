9e3de5d9b3708db28e5de825647fe602d6a3d2a1
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2015-03-31 14:15:33 +1100

    fixed class level requests

--- lib/her_extension/model/parse.rb
+++ lib/her_extension/model/parse.rb
@@ -1,5 +1,9 @@
 # TODO: PR to HER
 # Fix to_params when embeded_params is nil
+# Fix to_params when changes is nil 
+# --> allow all params - this is required to be able to make class level 
+#     requests like MyModel.post(path,{some: 'data'})
+#
 module Her
   module Model
     # This module handles resource data parsing at the model level (after the parsing middleware)
@@ -9,10 +13,10 @@ module Her
       module ClassMethods
 
         # @private
-        def to_params(attributes, changes={})
+        def to_params(attributes, changes = nil)
           filtered_attributes = attributes.dup.symbolize_keys
           filtered_attributes.merge!(embeded_params(attributes) || {})
-          if her_api.options[:send_only_modified_attributes]
+          if her_api.options[:send_only_modified_attributes] && !changes.nil?
             filtered_attributes = changes.symbolize_keys.keys.inject({}) do |hash, attribute|
               hash[attribute] = filtered_attributes[attribute]
               hash
