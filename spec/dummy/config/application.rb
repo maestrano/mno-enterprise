af342e013f554a11f33891ca4f09e3b3073d16f3
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2015-04-05 12:11:35 +1000

    fixed spec application

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -3,7 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 Bundler.require(*Rails.groups)
-require "mno_enterprise"
+require "mno-enterprise"
 
 module Dummy
   class Application < Rails::Application
