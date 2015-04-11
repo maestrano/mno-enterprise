232a2e40f09717da7e57af268f211d5023c48413
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2015-04-11 17:24:36 +1000

    added more install options

--- lib/generators/mno_enterprise/install/install_generator.rb
+++ lib/generators/mno_enterprise/install/install_generator.rb
@@ -22,6 +22,48 @@ module MnoEnterprise
         end
       end
       
+      def install_sprite_generator
+        default_answer = 'y'
+        @install_sprite = ask("Do you want to install sprite-factory? [Yn]")
+        @install_sprite = default_answer if @install_sprite.blank?
+        if @install_sprite =~ /y/i
+          gem "chunky_png", group: :development
+          gem "sprite-factory", group: :development
+          template "tasks/sprites.rake", "lib/tasks/sprites.rake"
+        end
+      end
+      
+      def install_rspec_rails
+        default_answer = 'y'
+        @install_rspec = ask("Do you want to install rspec-rails? [Yn]")
+        @install_rspec = default_answer if @install_rspec.blank?
+        if @install_rspec =~ /y/i
+          gem "rspec-rails", group: :test
+          generate "rspec:install"
+        end
+      end
+      
+      def install_rspec_rails
+        default_answer = 'y'
+        @install_facto_girl = ask("Do you want to install factory_girl_rails? [Yn]")
+        @install_facto_girl = default_answer if @install_facto_girl.blank?
+        if @install_facto_girl =~ /y/i
+          gem "factory_girl_rails", group: :test
+        end
+      end
+      
+      def install_summary
+        say("\n\n")
+        say("==> Maestrano Enterprise has been installed ==")
+        say("- You can generate deployment configs by running: 'rails g mno_enterprise:puma_stack'")
+        if @install_sprite =~ /y/i
+          say("\n\n")
+          say("==> Sprite Factory has been installed")
+          say("- Drop your icons in vendor/sprites/icons then run: 'rake assets:resprite'")
+          say("- Add more icons folders to vendor/sprites then modify lib/tasks/sprites.rake")
+        end
+        say("\n\n")
+      end
     end
   end
 end
