4a8364296b288be3606f75ce05a597a76a816f25
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2015-04-15 16:52:49 +1000

    added more config parameters for mnoe

--- lib/generators/mno_enterprise/templates/initializers/mno_enterprise.rb
+++ lib/generators/mno_enterprise/templates/initializers/mno_enterprise.rb
@@ -2,13 +2,38 @@ require 'mno-enterprise'
 
 MnoEnterprise.configure do |config|
   
-  # ==> Maestrano Enterprise Tenant Authentication
+  #===============================================
+  # General Configuration
+  #===============================================
+  # Name of your company/application
+  config.app_name = "My Company"
+  
+  #===============================================
+  # Maestrano Enterprise Tenant Authentication
+  #===============================================
   # Configure your tenant ID
   config.tenant_id = "my_tenant_id"
   
   # Configure your tenant Key
   config.tenant_key = "my_tenant_access_key"
   
+  #===============================================
+  # Emailing
+  #===============================================
+  # Mandrill API key for sending email 
+  # Defaulted to Maestrano Enterprise demo account
+  # config.mandrill_key = 'some-mandrill-api-key'
+  
+  # Support email address
+  config.support_email = 'support@example.com'
+  
+  # Default sender for system generated emails
+  config.default_sender_name = 'My Company'
+  config.default_sender_email = 'no-reply@example.com'
+  
+  #===============================================
+  # API Configuration
+  #===============================================
   # ==> Maestrano Enterprise API Configuration
   # Configure the API host and root path
   # config.mno_api_host = "https://api-enterprise.maestrano.com"
--- spec/dummy/config/initializers/mno_enterprise.rb
+++ spec/dummy/config/initializers/mno_enterprise.rb
@@ -1,17 +1,41 @@
 MnoEnterprise.configure do |config|
+    
+  #===============================================
+  # General Configuration
+  #===============================================
+  # Name of your company/application
+  config.app_name = "Enterprise Demo"
   
-  # ==> Maestrano Enterprise Tenant Authentication
+  #===============================================
+  # Maestrano Enterprise Tenant Authentication
+  #===============================================
   # Configure your tenant ID
   config.tenant_id = "ea461720-b044-0132-dba6-600308937d74"
   
   # Configure your tenant Key
   config.tenant_key = "dPhCSjZCJ68I2cQLzCBtTg"
   
+  #===============================================
+  # Emailing
+  #===============================================
+  # Mandrill API key for sending email 
+  # Defaulted to Maestrano Enterprise demo account
+  # config.mandrill_key = 'some-mandrill-api-key'
+  
+  # Support email address
+  config.support_email = 'support@enteprise-demo-mnoe.maestrano.io'
+  
+  # Default sender for system generated emails
+  config.default_sender_name = 'Enterprise Demo'
+  config.default_sender_email = 'no-reply@enteprise-demo-mnoe.maestrano.io'
+  
+  #===============================================
+  # API Configuration
+  #===============================================
   # ==> Maestrano Enterprise API Configuration
   # Configure the API host and root path
   config.mno_api_host = "http://localhost:3000"
   
   # Configure the API root path
   config.mno_api_root_path = "/api/mnoe/v1"
-  
 end
