c8b8672617953573d843683aaf088b731029b6c9
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2015-04-15 21:41:51 +1000

    changed host

--- spec/dummy/config/environments/development.rb
+++ spec/dummy/config/environments/development.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   config.assets.debug = true
   
   # Specify host to use when sending emails
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'localhost:7000' }
   
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
