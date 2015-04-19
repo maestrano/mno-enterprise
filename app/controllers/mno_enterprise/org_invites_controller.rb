f81514aa9af3239302109902338ffa3cabb4eeb2
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2015-04-19 16:20:41 +1000

    fixed redirection

--- app/controllers/mno_enterprise/org_invites_controller.rb
+++ app/controllers/mno_enterprise/org_invites_controller.rb
@@ -6,16 +6,19 @@ module MnoEnterprise
     def show
       @current_user = current_user
       @org_invite = MnoEnterprise::OrgInvite.active.where(id: params[:id], token: params[:token]).first
+      redirect_path = myspace_path
       
       if @org_invite && !@org_invite.expired? && @org_invite.accept!(current_user)
+        redirect_path += "#/?dhbRefId=#{ @org_invite.organization.id}"
         message = { notice: "You are now part of #{@org_invite.organization.name}" }
       elsif @org_invite && @org_invite.expired?
         message = { alert: "It looks like this invite has expired. Please ask your company administrator to resend the invite." }
       else
         message = { alert: "Unfortunately, this invite does not seem to be valid." }
       end
+      puts redirect_path
       
-      redirect_to myspace_path, message
+      redirect_to redirect_path, message
     end
     
   end
