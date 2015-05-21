95cf1a690f9253e14f9c009889d286db866cee32
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2015-05-21 21:03:34 +1000

    fixed some styling - fixed login after confirmation

--- app/controllers/mno_enterprise/user_setup_controller.rb
+++ app/controllers/mno_enterprise/user_setup_controller.rb
@@ -1,6 +1,6 @@
 module MnoEnterprise
   class UserSetupController < ApplicationController
-    before_filter :authenticate_user_or_signup!
+    before_filter :authenticate_user!
     
     # GET /user_setup/:id
     # Display a specific step for the user setup
