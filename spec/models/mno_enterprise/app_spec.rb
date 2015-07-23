a61c92638d59ad324844582017aad3ef1962a3fe
Olivier Brisse <olivier.brisse@maestrano.com>
2015-07-23 18:32:49 +1000

    Fix specs

--- spec/models/mno_enterprise/app_spec.rb
+++ spec/models/mno_enterprise/app_spec.rb
@@ -7,7 +7,7 @@ module MnoEnterprise
       let(:app) { build(:app, description: "Some description by Maestrano") }
       
       it 'replaces any mention of maestrano by the name of the platform' do
-        expect(app.sanitized_description).to eq("Some description by #{MnoEnterprise.app_name}")
+        expect(app.sanitized_description).to eq("Some description by #{MnoEnterprise.app_name}")
       end
     end
   end
