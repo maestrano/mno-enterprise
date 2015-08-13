22d35b2af68ea4d7b163d266368258d35c5e5c1a
Olivier Brisse <olivier.brisse@maestrano.com>
2015-08-13 10:48:22 +1000

    Make Jpi::OrganizationsController decorator ready

--- api/lib/mno_enterprise/api/engine.rb
+++ api/lib/mno_enterprise/api/engine.rb
@@ -2,6 +2,8 @@ module MnoEnterprise
   module Api
     class Engine < ::Rails::Engine
       isolate_namespace MnoEnterprise
+      # To be able to load lib/mno_enterprise/concerns/...
+      config.autoload_paths += Dir["#{config.root}/lib/**/"]
     end
   end
 end
