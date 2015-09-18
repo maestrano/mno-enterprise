f2814470da3f81fc8290613ca7aa313da1820943
Olivier Brisse <olivier.brisse@maestrano.com>
2015-09-18 10:59:45 +1000

    Decorate ProvisionController
    Allow after_provision_path customization

--- api/app/controllers/mno_enterprise/provision_controller.rb
+++ api/app/controllers/mno_enterprise/provision_controller.rb
@@ -1,36 +1,5 @@
 module MnoEnterprise
   class ProvisionController < ApplicationController
-    before_filter :authenticate_user_or_signup!
-    
-    # GET /provision/new?apps[]=vtiger&organization_id=1
-    # TODO: check organization accessibility via ability
-    def new
-      @apps = params[:apps]
-      @organizations = current_user.organizations.to_a
-      @organization = @organizations.find { |o| o.id && o.id.to_s == params[:organization_id].to_s }
-      
-      unless @organization
-        @organization = @organizations.one? ? @organizations.first : nil
-      end
-      
-      # Redirect to dashboard if no applications
-      unless @apps && @apps.any?
-        redirect_to myspace_path
-      end
-    end
-    
-    # POST /provision
-    # TODO: check organization accessibility via ability
-    def create
-      @organization = current_user.organizations.to_a.find { |o| o.id && o.id.to_s == params[:organization_id].to_s }
-      
-      app_instances = []
-      params[:apps].each do |product_name|
-        app_instances << @organization.app_instances.create(product: product_name)
-      end
-      
-      head :created
-    end
-      
+    include MnoEnterprise::Concerns::Controllers::ProvisionController
   end
 end
