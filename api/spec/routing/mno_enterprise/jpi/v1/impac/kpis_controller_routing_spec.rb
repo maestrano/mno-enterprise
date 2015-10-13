dc8255d4b4282e6b04b26d55ca71baf19c048bfd
Olivier Brisse <olivier.brisse@maestrano.com>
2015-10-13 11:54:12 +1100

    Style fix

--- api/spec/routing/mno_enterprise/jpi/v1/impac/kpis_controller_routing_spec.rb
+++ api/spec/routing/mno_enterprise/jpi/v1/impac/kpis_controller_routing_spec.rb
@@ -5,16 +5,16 @@ module MnoEnterprise
     routes { MnoEnterprise::Engine.routes }
    
     describe 'collection routes (dashboard nested)' do
-      it "routes to #update" do
-        put("/jpi/v1/impac/kpis/2").should route_to("mno_enterprise/jpi/v1/impac/kpis#update", id: '2')
+      it 'routes to #update' do
+        expect(put('/jpi/v1/impac/kpis/2')).to route_to('mno_enterprise/jpi/v1/impac/kpis#update', id: '2')
       end
 
-      it "routes to #destroy" do
-        delete("/jpi/v1/impac/kpis/2").should route_to("mno_enterprise/jpi/v1/impac/kpis#destroy", id: '2')
+      it 'routes to #destroy' do
+        expect(delete('/jpi/v1/impac/kpis/2')).to route_to('mno_enterprise/jpi/v1/impac/kpis#destroy', id: '2')
       end
 
-      it "routes to #create" do
-        post("/jpi/v1/impac/dashboards/1/kpis").should route_to("mno_enterprise/jpi/v1/impac/kpis#create", dashboard_id: '1')
+      it 'routes to #create' do
+        expect(post('/jpi/v1/impac/dashboards/1/kpis')).to route_to('mno_enterprise/jpi/v1/impac/kpis#create', dashboard_id: '1')
       end
     end
 
