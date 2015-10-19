4d682a7fd4417044dc7e1e8bae54f1ddac4fd260
Cesar Tonnoir <cesar.tonnoir@gmail.com>
2015-10-20 02:26:21 +1100

    add belongs_to relations for kpis and widgets

--- app/models/mno_enterprise/impac/kpi.rb
+++ app/models/mno_enterprise/impac/kpi.rb
@@ -2,6 +2,8 @@ module MnoEnterprise
   class Impac::Kpi < BaseResource
 
     attributes :name, :settings, :target, :extra_param, :endpoint, :source, :element_watched
+    
+    belongs_to :impac_dashboard, class_name: 'MnoEnterprise::Impac::Dashboard'
 
   end
 end
--- app/models/mno_enterprise/impac/widget.rb
+++ app/models/mno_enterprise/impac/widget.rb
@@ -3,5 +3,7 @@ module MnoEnterprise
 
   	attributes :name, :width, :widget_category, :settings
 
+    belongs_to :impac_dashboard, class_name: 'MnoEnterprise::Impac::Dashboard'
+  
   end
 end
