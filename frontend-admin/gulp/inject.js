298c83b5748a9de54b8de703f9845b005e76f94c
Alexandre Jarnoux <alex.jarnoux@gmail.com>
2015-11-11 15:41:55 +1100

    Commenting angular file sort

--- frontend-admin/gulp/inject.js
+++ frontend-admin/gulp/inject.js
@@ -24,8 +24,8 @@ gulp.task('inject', ['scripts', 'styles'], function () {
     path.join(conf.paths.tmp, '/serve/app/**/*.js'),
     path.join('!' + conf.paths.tmp, '/serve/app/**/*.spec.js'),
     path.join('!' + conf.paths.tmp, '/serve/app/**/*.mock.js')
-  ])
-  .pipe($.angularFilesort()).on('error', conf.errorHandler('AngularFilesort'));
+  ]);
+  //.pipe($.angularFilesort()).on('error', conf.errorHandler('AngularFilesort'));
 
   var injectOptions = {
     ignorePath: [conf.paths.src, path.join(conf.paths.tmp, '/serve')],
