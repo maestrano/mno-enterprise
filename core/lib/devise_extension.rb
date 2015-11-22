58139c77b987ede393c5c6d1132b589f2b24df30
Olivier Brisse <olivier.brisse@maestrano.com>
2015-11-23 10:29:40 +1100

    Password Complexity check

--- core/lib/devise_extension.rb
+++ core/lib/devise_extension.rb
@@ -6,6 +6,15 @@ module Devise
   # Should the password expire (e.g 3.months)
   mattr_accessor :expire_password_after
   @@expire_password_after = 3.months
+
+  # Validate password strength
+  mattr_accessor :password_regex
+  @@password_regex = nil
+  # Need 1 char of A-Z, a-z and 0-9
+  # @@password_regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/
+
+  mattr_accessor :password_regex_message
+  @@password_regex_message = 'must contains at least one uppercase letter, one lower case letter and a number'
 end
 
 # an security extension for devise
--- core/spec/models/mno_enterprise/user_spec.rb
+++ core/spec/models/mno_enterprise/user_spec.rb
@@ -2,6 +2,43 @@ require 'rails_helper'
 
 module MnoEnterprise
   RSpec.describe User, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
+    describe 'password strength' do
+      let(:user) do
+        # Initialize this way so the class reload is taken into account (the factory doesnt reload the User class)
+        MnoEnterprise::User.new(attributes_for(:user, password: 'password')).tap {|u| u.clear_attribute_changes!}
+      end
+
+      context 'without password regex' do
+        it 'does not validate the password strength' do
+          expect(user).to be_valid
+        end
+      end
+
+      context 'with password regex' do
+        before do
+          Devise.password_regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/
+
+          # Reload User class to redefine the validation
+          # Removes MnoEnterprise::User from object-space:
+          MnoEnterprise.send(:remove_const, :User)
+          # Reloads the module (require might also work):
+          load 'app/models/mno_enterprise/user.rb'
+        end
+
+        it 'validates the password strength' do
+          expect(user).to be_invalid
+          expect(user.errors[:password].first).to eq('must contains at least one uppercase letter, one lower case letter and a number')
+        end
+
+        after do
+          # Reset to default
+          Devise.password_regex = nil
+          MnoEnterprise.send(:remove_const, :User)
+          load 'app/models/mno_enterprise/user.rb'
+        end
+      end
+
+    end
+
   end
 end
