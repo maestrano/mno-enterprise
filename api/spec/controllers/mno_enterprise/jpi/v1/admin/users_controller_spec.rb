028755b10e14b871bdcf209e6d150264f4bd5692
Gaetan Gueraud <gaetan.gueraud@maestrano.com>
2015-12-01 15:51:09 +1100

    fix(TeamAddUser): fix add of multiples users
     -add specs for the action

--- api/spec/controllers/mno_enterprise/jpi/v1/admin/users_controller_spec.rb
+++ api/spec/controllers/mno_enterprise/jpi/v1/admin/users_controller_spec.rb
@@ -6,6 +6,17 @@ module MnoEnterprise
     routes { MnoEnterprise::Engine.routes }
     before { request.env["HTTP_ACCEPT"] = 'application/json' }
 
+    def partial_hash_for_organization(user)
+      user.organizations.map do |org|
+        {
+            'id' => org.id,
+            'uid' => org.uid,
+            'name' => org.name,
+            'created_at' => org.created_at
+        }
+      end
+    end
+
     def partial_hash_for_user(user)
       {
           'id' => user.id,
@@ -17,7 +28,8 @@ module MnoEnterprise
           'admin_role' => user.admin_role,
           'created_at' => user.created_at,
           'last_sign_in_at' => user.last_sign_in_at,
-          'confirmed_at' => user.confirmed_at
+          'confirmed_at' => user.confirmed_at,
+          'organizations' => partial_hash_for_organization(user)
       }
     end
 
@@ -50,15 +62,12 @@ module MnoEnterprise
     #===============================================
     # Assignments
     #===============================================
-    # Stub controller ability
-    # let!(:ability) { stub_ability }
-    # before { allow(ability).to receive(:can?).with(any_args).and_return(true) }
-
     # Stub user and user call
-    let(:user) { build(:user, :admin) }
+    let(:user) { build(:user, :admin, :with_organizations) }
     before do
       api_stub_for(get: "/users", response: from_api([user]))
       api_stub_for(get: "/users/#{user.id}", response: from_api(user))
+      api_stub_for(get: "/users/#{user.id}/organizations", response: from_api(user))
       sign_in user
     end
 
