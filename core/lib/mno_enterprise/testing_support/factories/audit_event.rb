810b82079d84be7de9c76c2168c40e8e3b2bba76
Olivier Brisse <olivier.brisse@maestrano.com>
2015-12-01 19:06:56 +1100

    Fix specs

--- core/lib/mno_enterprise/testing_support/factories/audit_event.rb
+++ core/lib/mno_enterprise/testing_support/factories/audit_event.rb
@@ -5,6 +5,7 @@ FactoryGirl.define do
       sequence(:key) { |n| "event-fab3#{n}" }
       user_id 1
       description 'Blabla'
+      user { {name: 'John', surname: 'Doe'} }
 
       # Properly build the resource with Her
       initialize_with { new(attributes).tap { |e| e.clear_attribute_changes! } }
