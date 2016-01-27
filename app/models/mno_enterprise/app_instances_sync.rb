c3e713a19e3d276507a7246d0765a3da9575330f
Cesar Tonnoir <cesar.tonnoir@gmail.com>
2016-01-27 17:33:32 +1100

    remove app/ created after cherry-pick

--- app/models/mno_enterprise/app_instances_sync.rb
+++
@@ -1,6 +0,0 @@
-module MnoEnterprise
-  class AppInstancesSync < BaseResource
-    attributes :errors, :connectors, :mode
-    belongs_to :organization, class_name: 'MnoEnterprise::Organization'
-  end
-end
