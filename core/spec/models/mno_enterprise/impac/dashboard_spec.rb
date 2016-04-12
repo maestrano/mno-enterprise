maestrano/mno-enterprise

commit 552c0b7bac5bdcd2fe08ba34eadb80f6598580ce

Enabled widget list filtering

api/app/views/mno_enterprise/jpi/v1/impac/dashboards/_dashboard.json.jbuilder
@@ -5,5 +5,5 @@ json.data_sources dashboard.organizations.compact.map do |org|
   json.uid org.uid
   json.label org.name
 end
-json.widgets dashboard.sorted_widgets, partial: 'mno_enterprise/jpi/v1/impac/widgets/widget', as: :widget
+json.widgets dashboard.filtered_widgets_templates, partial: 'mno_enterprise/jpi/v1/impac/widgets/widget', as: :widget
 json.kpis dashboard.kpis, partial: 'mno_enterprise/jpi/v1/impac/kpis/kpi', as: :kpi

core/app/models/mno_enterprise/impac/dashboard.rb
@@ -3,31 +3,42 @@ module MnoEnterprise
 
     attributes :name, :widgets_order, :organization_ids, :widgets_templates, :currency
 
-	  has_many :widgets, class_name: 'MnoEnterprise::Impac::Widget', dependent: :destroy
-	  has_many :kpis, class_name: 'MnoEnterprise::Impac::Kpi', dependent: :destroy
-	  belongs_to :owner, polymorphic: true
-	  
-	  #============================================
-	  # Instance methods
-	  #============================================
-	  # Return the full name of this dashboard
-	  # Currently a simple accessor to the dashboard name (used to include the company name)
-	  def full_name
-	    self.name
-	  end
-	  
-	  # Return all the organizations linked to this dashboard and to which
-	  # the user has access
-	  def organizations
-	  	self.organization_ids.map do |uid|
-	    	MnoEnterprise::Organization.find_by(uid: uid)
-	  	end
-	  end
+    has_many :widgets, class_name: 'MnoEnterprise::Impac::Widget', dependent: :destroy
+    has_many :kpis, class_name: 'MnoEnterprise::Impac::Kpi', dependent: :destroy
+    belongs_to :owner, polymorphic: true
 
-	  def sorted_widgets
-	    order = self.widgets_order.map(&:to_i) | self.widgets.map{|w| w.id }
-	    order.map { |id| self.widgets.to_a.find{ |w| w.id == id} }.compact
-	  end
+    #============================================
+    # Instance methods
+    #============================================
+    # Return the full name of this dashboard
+    # Currently a simple accessor to the dashboard name (used to include the company name)
+    def full_name
+      self.name
+    end
+
+    # Return all the organizations linked to this dashboard and to which
+    # the user has access
+    def organizations
+      self.organization_ids.map do |uid|
+        MnoEnterprise::Organization.find_by(uid: uid)
+      end
+    end
+
+    def sorted_widgets
+      order = self.widgets_order.map(&:to_i) | self.widgets.map{|w| w.id }
+      order.map { |id| self.widgets.to_a.find{ |w| w.id == id} }.compact
+    end
+
+    # Filter widgets list based on config
+    def filtered_widgets_templates
+      if MnoEnterprise.widgets_templates_listing
+        return self.widgets_templates.select do |t|
+          MnoEnterprise.widgets_templates_listing.include?(t[:path])
+        end
+      else
+        return self.widgets_templates
+      end
+    end
 
     def to_audit_event
       name

core/lib/generators/mno_enterprise/install/templates/config/initializers/mno_enterprise.rb
@@ -135,4 +135,54 @@ MnoEnterprise.configure do |config|
   #   "xero",
   #   "wordpress"
   # ]
+
+  #====================================
+  # Impac! widgets templates listing
+  #====================================
+  # config.widgets_templates_listing = nil
+  # config.widgets_templates_listing = [
+  #   'accounts/balance',
+  #   'accounts/comparison',
+  #   'accounts/expenses_revenue',
+  #   'accounts/payable_receivable',
+  #   'accounts/assets_summary',
+  #   'accounts/custom_calculation',
+  #   'accounts/accounting_values/ebitda',
+  #   'accounts/accounting_values/turnover',
+  #   'accounts/accounting_values/workforce_costs',
+  #   'accounts/accounting_values/payroll_taxes_account',
+  #   'accounts/cash_summary',
+  #   'accounts/balance_sheet',
+  #   'accounts/profit_and_loss',
+  #   'invoices/list',
+  #   'invoices/summary',
+  #   'invoices/aged_payables_receivables',
+  #   'hr/workforce_summary',
+  #   'hr/salaries_summary',
+  #   'hr/employees_list',
+  #   'hr/employee_details',
+  #   'hr/payroll_taxes',
+  #   'hr/superannuation_accruals',
+  #   'hr/leaves_balance',
+  #   'hr/payroll_summary',
+  #   'hr/timesheets',
+  #   'sales/summary',
+  #   'sales/list',
+  #   'sales/growth',
+  #   'sales/segmented_turnover',
+  #   'sales/customer_details',
+  #   'sales/margin',
+  #   'sales/aged',
+  #   'sales/comparison',
+  #   'sales/leads_list',
+  #   'sales/number_of_leads',
+  #   'sales/cycle',
+  #   'sales/leads_funnel',
+  #   'sales/opportunities_funnel',
+  #   'sales/top_opportunities',
+  #   'sales/break_even',
+  #   'sales/forecast',
+  #   'sales/performance'
+  # ]
+
 end

core/lib/mno_enterprise/core.rb
@@ -211,6 +211,14 @@ module MnoEnterprise
   @@marketplace_listing = nil
 
   #====================================
+  # Impac! widgets templates listing
+  #====================================
+  # List of widget templates that should be offered on Impac!
+  # if nil, all widget templates available will be offered
+  mattr_accessor :widgets_templates_listing
+  @@widgets_templates_listing = nil
+
+  #====================================
   # Module Methods
   #====================================
 

commit 0a01eef22b61d1f9a6e1d8e2bb9568bcf3214848

[MNOE-64] Fix bug introduced in 552c0b7b

api/app/views/mno_enterprise/jpi/v1/impac/dashboards/_dashboard.json.jbuilder
@@ -1,9 +1,10 @@
-json.extract! dashboard, :id, :name, :full_name, :widgets_templates, :currency
+json.extract! dashboard, :id, :name, :full_name, :currency
 
 json.data_sources dashboard.organizations.compact.map do |org|
   json.id org.id
   json.uid org.uid
   json.label org.name
 end
-json.widgets dashboard.filtered_widgets_templates, partial: 'mno_enterprise/jpi/v1/impac/widgets/widget', as: :widget
+json.widgets dashboard.sorted_widgets, partial: 'mno_enterprise/jpi/v1/impac/widgets/widget', as: :widget
+json.widgets_templates dashboard.filtered_widgets_templates
 json.kpis dashboard.kpis, partial: 'mno_enterprise/jpi/v1/impac/kpis/kpi', as: :kpi

core/spec/models/mno_enterprise/impac/dashboard_spec.rb
@@ -1,0 +1,46 @@
+require 'rails_helper'
+
+module MnoEnterprise
+  RSpec.describe Impac::Dashboard, type: :model do
+    subject(:dashboard) { build(:impac_dashboard) }
+
+    describe '#full_name' do
+      subject { dashboard.full_name }
+      it { is_expected.to eq(dashboard.name) }
+    end
+
+    describe '#sorted_widgets' do
+      it 'is pending'
+    end
+
+    describe '#filtered_widgets_templates' do
+      let(:templates) {
+        [
+          {path: 'accounts/balance', name: 'Account balance'},
+          {path: 'accounts/comparison', name: 'Accounts comparison'}
+        ]
+      }
+      subject(:dashboard) { build(:impac_dashboard, widgets_templates: templates) }
+
+      subject { dashboard.filtered_widgets_templates }
+
+      context 'with no filter' do
+        before { MnoEnterprise.widgets_templates_listing = nil}
+
+        it 'returns all the widgets' do
+          expect(subject).to eq(templates)
+        end
+      end
+
+      context 'with a filter' do
+        let(:filter) { ['accounts/balance'] }
+        before { MnoEnterprise.widgets_templates_listing = filter }
+
+        it 'returns a filtered list' do
+          expect(subject.size).to eq(1)
+          expect(subject.map{|h| h[:path]}).to eq(filter)
+        end
+      end
+    end
+  end
+end