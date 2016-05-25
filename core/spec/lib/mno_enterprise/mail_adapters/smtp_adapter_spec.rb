3771db0afc722be2b63eb66822147bce58665d40
PCoder <mobile.passion2015@yandex.com>
2016-05-25 05:35:06 +0200

    #Update - Spec

--- core/spec/lib/mno_enterprise/mail_adapters/smtp_adapter_spec.rb
+++ core/spec/lib/mno_enterprise/mail_adapters/smtp_adapter_spec.rb
@@ -1,21 +1,23 @@
 require "rails_helper"
 
 describe MnoEnterprise::MailAdapters::SmtpAdapter do
-	subject { described_class }
+  describe '.deliver' do
+    before { described_class.instance_variable_set("@client", nil) }
 
-	describe '.deliver' do
-		before { described_class.instance_variable_set("@client", nil) }
+    let(:template) { :some_template }
+    let(:from) {{ name: "John", email: "j@e.com" }}
+    let(:to) {{ name: "John", email: "j@e.com" }}
+    let(:vars) {{ some: 'var' }}
 
-		let(:template) { :some_template }
-		let(:from) {{ name: "John", email: "j@e.com" }}
-		let(:to) {{ name: "John", email: "j@e.com" }}
-		let(:vars) {{ some: 'var' }}
+    subject { described_class.deliver(template, from, to, vars) }   
 
-		subject { described_class.deliver(template, from, to, vars)}
-		
-
-		it 'delegates the method to a smtp client' do
-			
-		end
-	end
+    it 'delegates the method to a smtp client' do
+      # Stub SmptClient
+      smtp = double('smtpclient')
+      expect(MnoEnterprise::SmtpClient).to receive(:new).and_return(smtp)
+      
+      expect(smtp).to receive(:deliver).with(template, from, to, vars)
+      subject
+    end
+  end
 end
