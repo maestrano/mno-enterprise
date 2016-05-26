f582f1ee30aae602f5ef0316a6dfadc78cf3b1f5
PCoder <mobile.passion2015@yandex.com>
2016-05-26 04:08:27 +0200

    #Update - SMTP_ADAPTER_SPEC

--- core/spec/lib/mno_enterprise/mail_adapters/smtp_adapter_spec.rb
+++ core/spec/lib/mno_enterprise/mail_adapters/smtp_adapter_spec.rb
@@ -8,16 +8,22 @@ describe MnoEnterprise::MailAdapters::SmtpAdapter do
     let(:from) {{ name: "John", email: "j@e.com" }}
     let(:to) {{ name: "John", email: "j@e.com" }}
     let(:vars) {{ some: 'var' }}
+    let(:opts) {{ some: 'opt' }}
 
-    subject { described_class.deliver(template, from, to, vars) }   
+    subject { described_class.deliver(template, from, to, vars, opts) }   
 
     it 'delegates the method to a smtp client' do
       # Stub SmptClient
       smtp = double('smtpclient')
+      smtp_mail = double('smtpmail')
+      
       expect(MnoEnterprise::SmtpClient).to receive(:new).and_return(smtp)
       
-      expect(smtp).to receive(:deliver).with(template, from, to, vars)
+      expect(smtp).to receive(:deliver).with(template, from, to, vars, opts).and_return(smtp_mail)
+      expect(smtp_mail).to receive(:deliver)
+
       subject
     end
   end
 end
+
