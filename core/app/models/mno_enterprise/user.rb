8bd1dfe9dbea71d2cb0a6d00b9eeac1b8710b0f3
x4d3 <git@xade.eu>
2016-09-19 17:36:52 +0100

    Add OmniAuth

--- core/app/models/mno_enterprise/user.rb
+++ core/app/models/mno_enterprise/user.rb
@@ -1,6 +1,6 @@
 # == Schema Information
 #
-# Endpoint: 
+# Endpoint:
 #   - /v1/users
 #   - /v1/organizations/:organization_id/users
 #
@@ -27,31 +27,32 @@
 #  created_at                     :datetime        not null
 #  updated_at                     :datetime        not null
 #  name                           :string(255)
-#  surname                        :string(255)            
-#  company                        :string(255)   
+#  surname                        :string(255)
+#  company                        :string(255)
 #  phone                          :string(255)
 #  phone_country_code             :string(255)
 #  geo_country_code               :string(255)
 #  geo_state_code                 :string(255)
-#  geo_city                       :string(255)  
-#  website                        :string(255)  
+#  geo_city                       :string(255)
+#  website                        :string(255)
 #
 
 module MnoEnterprise
   class User < BaseResource
     extend Devise::Models
-    
+
     # Note: password and encrypted_password are write-only attributes and are never returned by
     # the remote API. If you are looking for a session token, use authenticatable_salt
-    attributes :uid, :email, :password, :current_password, :password_confirmation, :authenticatable_salt, :encrypted_password, :reset_password_token, :reset_password_sent_at, 
-      :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, 
-      :last_sign_in_ip, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, 
-      :failed_attempts, :unlock_token, :locked_at, :name, :surname, :company, :phone, :phone_country_code, 
+    attributes :uid, :email, :password, :current_password, :password_confirmation, :authenticatable_salt, :encrypted_password, :reset_password_token, :reset_password_sent_at,
+      :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip,
+      :last_sign_in_ip, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email,
+      :failed_attempts, :unlock_token, :locked_at, :name, :surname, :company, :phone, :phone_country_code,
       :geo_country_code, :geo_state_code, :geo_city, :website, :orga_on_create, :sso_session, :current_password_required, :admin_role
-    
+
     define_model_callbacks :validation #required by Devise
     devise :remote_authenticatable, :registerable, :recoverable, :rememberable,
-      :trackable, :validatable, :lockable, :confirmable, :timeoutable, :password_expirable
+      :trackable, :validatable, :lockable, :confirmable, :timeoutable, :password_expirable,
+           :omniauthable, omniauth_providers: [:intuit, :linkedin, :google, :facebook]
 
     #================================
     # Validation
@@ -83,25 +84,25 @@ module MnoEnterprise
     # Return nil in case of failure
     def self.authenticate(auth_hash)
       u = self.post("user_sessions", auth_hash)
-      
+
       if u && u.id
         u.clear_attribute_changes!
         return u
       end
-      
+
       nil
     end
-    
+
     #================================
     # Devise Confirmation
     # TODO: should go in a module
     #================================
-    
-    
+
+
     # Override Devise to allow confirmation via original token
     # Less secure but useful if user has been created by Maestrano Enterprise
     # (happens when an orga_invite is sent to a new user)
-    #  
+    #
     # Find a user by its confirmation token and try to confirm it.
     # If no user is found, returns a new user with an error.
     # If the user is already confirmed, create an error for the user
@@ -111,23 +112,23 @@ module MnoEnterprise
       confirmable.perform_confirmation(confirmation_token)
       confirmable
     end
-    
+
     # Find a user using a confirmation token
     def self.find_for_confirmation(confirmation_token)
       original_token     = confirmation_token
       confirmation_token = Devise.token_generator.digest(self, :confirmation_token, confirmation_token)
-      
+
       confirmable = find_or_initialize_with_error_by(:confirmation_token, confirmation_token)
       confirmable = find_or_initialize_with_error_by(:confirmation_token, original_token) if confirmable.errors.any?
       confirmable
     end
-    
+
     # Confirm the user and store confirmation_token
     def perform_confirmation(confirmation_token)
       self.confirm if self.persisted?
       self.confirmation_token = confirmation_token
     end
-    
+
     # It may happen that that the errors attribute become nil, which breaks the controller logic (rails responder)
     # This getter ensures that 'errors' is always initialized
     def errors
@@ -159,18 +160,18 @@ module MnoEnterprise
     def failed_attempts
       read_attribute(:failed_attempts) || 0
     end
-    
+
     # Override Devise default method
     def authenticatable_salt
       read_attribute(:authenticatable_salt)
     end
-    
+
     # Return the role of this user for the provided
     # organization
     def role(organization = nil)
       # Return cached version if available
       return self.read_attribute(:role) if !organization
-      
+
       org = self.organizations.to_a.find { |o| o.id.to_s == organization.id.to_s }
       org ? org.role : nil
     end
@@ -184,5 +185,70 @@ module MnoEnterprise
       self.reload
       Rails.cache.write(['user', self.to_key], self)
     end
+
+    # Used by omniauth providers to find or create users
+    # on maestrano
+    # See Auth::OmniauthCallbacksController
+    def self.find_for_oauth(auth, opts = {}, signed_in_resource = nil)
+      # Get the identity and user if they exist
+      identity = Identity.find_for_oauth(auth)
+
+      # If a signed_in_resource is provided it always overrides the existing user
+      # to prevent the identity being locked with accidentally created accounts.
+      # Note that this may leave zombie accounts (with no associated identity) which
+      # can be cleaned up at a later date.
+      user = signed_in_resource ? signed_in_resource : identity.user
+
+      # Create the user if needed
+      if user.nil?
+        # Get the existing user by email.
+        email = auth.info.email
+        user = User.where(email: email).first if email
+
+        # Create the user if it's a new registration
+        if user.nil?
+          user = create_from_omniauth(auth, opts.except(:authorized_link_to_email))
+        elsif auth.provider == 'intuit'
+          unless opts[:authorized_link_to_email] == user.email
+            # Intuit email is NOT a confirmed email. Therefore we need to ask the user to
+            # login the old fashion to make sure it is the right user!
+            fail(SecurityError, 'reconfirm credentials')
+          end
+        end
+      end
+
+      # Associate the identity with the user if needed
+      if identity.user != user
+        identity.user = user
+        identity.save!
+      end
+      user
+    end
+
+    # Create a new user from omniauth hash
+    def self.create_from_omniauth(auth, opts = {})
+      user = User.new(
+        name: auth.info.first_name.presence || auth.info.email[/(\S*)@/, 1],
+        surname: auth.info.last_name.presence || '',
+        email: auth.info.email,
+        password: Devise.friendly_token[0, 20],
+        avatar_url: auth.info.image.presence
+      )
+
+      # To force user to enter company and user details when accessing the dashboard
+      user.metadata ||= {}
+      user.metadata.merge!(need_details_update: {others: true})
+
+      # opts hash is expected to contain additional attributes
+      # to set on the model
+      user.assign_attributes(opts)
+
+      # Skip email confirmation if not from Intuit (Intuit email is NOT a confirmed email)
+      user.skip_confirmation! unless auth.provider == 'intuit'
+      user.save!
+
+      user
+    end
+
   end
 end
