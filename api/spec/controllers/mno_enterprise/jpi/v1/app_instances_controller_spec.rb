0a385e4823135a651689832892c791e5ecf42c08
Olivier Brisse <olivier.brisse@maestrano.com>
2016-10-24 18:55:50 +1100

    [PERF] Filter AppInstances using Her scope

--- api/spec/controllers/mno_enterprise/jpi/v1/app_instances_controller_spec.rb
+++ api/spec/controllers/mno_enterprise/jpi/v1/app_instances_controller_spec.rb
@@ -15,14 +15,22 @@ module MnoEnterprise
     # Stub user and user call
     let(:user) { build(:user) }
     before { api_stub_for(get: "/users/#{user.id}", response: from_api(user)) }
-
-    # Stub organization/app_instance + associations
+    # Stub organization + associations
     let(:organization) { build(:organization) }
-    let(:app_instance) { build(:app_instance, status: "running") }
     before { allow_any_instance_of(MnoEnterprise::User).to receive(:organizations).and_return([organization]) }
-    before { allow(organization).to receive(:app_instances).and_return([app_instance]) }
 
     describe 'GET #index' do
+      let(:app_instance) { build(:app_instance, status: "running") }
+      let(:app_instances) { instance_double('Her::Model::Relation') }
+
+      before do
+        allow(organization).to receive(:app_instances).and_return(app_instances)
+        # Only active instances
+        allow(app_instances).to receive(:active).and_return(app_instances)
+        # Updated since last tick
+        allow(app_instances).to receive(:where).and_return([app_instance])
+      end
+
       before { sign_in user }
       let(:timestamp) { nil }
       subject { get :index, organization_id: organization.id, timestamp: timestamp }
@@ -30,35 +38,40 @@ module MnoEnterprise
       it_behaves_like "jpi v1 protected action"
 
       describe 'all' do
-        before { subject }
-        it { expect(assigns(:app_instances)).to eq([app_instance]) }
+        it { subject; expect(assigns(:app_instances)).to eq([app_instance]) }
+
+        it 'filter only active instances' do
+          expect(app_instances).to receive(:active)
+          subject
+        end
       end
 
-      describe 'with inactive app_instances' do
-        let(:app_instance) { build(:app_instance, status: "terminated", terminated_at: 10.minutes.ago) }
-        before { subject }
-        it { expect(assigns(:app_instances)).to be_empty }
+      context 'with timestamp' do
+        let(:timestamp) { Time.current.to_i }
+
+        it 'filter updates since last request' do
+          expect(app_instances).to receive(:where).with("updated_at.gt" => Time.at(timestamp))
+          subject
+        end
       end
 
-      describe 'with no access to the app_instance' do
+      context 'without access to the app_instance' do
         before { allow(ability).to receive(:can?).with(any_args).and_return(false) }
         before { subject }
         it { expect(assigns(:app_instances)).to be_empty }
       end
+    end
 
-      describe 'with timestamp' do
-        describe 'before instance updated_at' do
-          let(:timestamp) { (app_instance.updated_at - 2.minutes).to_i }
-          before { subject }
-          it { expect(assigns(:app_instances)).to eq([app_instance]) }
-        end
+    describe 'POST #create' do
+      subject { post :create, organization_id: organization.id, nid: 'my-app' }
 
-        describe 'after instance updated_at' do
-          let(:timestamp) { (app_instance.updated_at + 2.minutes).to_i }
-          before { subject }
-          it { expect(assigns(:app_instances)).to be_empty }
-        end
+      before do
+        api_stub_for(post: "/organizations/#{organization.id}/app_instances")
+        api_stub_for(get: "/organizations/#{organization.id}/app_instances")
       end
+
+
+      it_behaves_like "jpi v1 protected action"
     end
 
     describe 'DELETE #destroy' do
