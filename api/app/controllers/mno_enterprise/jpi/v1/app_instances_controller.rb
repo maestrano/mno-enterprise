58c4df1dc005bd11198cc808017b181b344bb596
Olivier Brisse <olivier.brisse@maestrano.com>
2016-10-24 11:23:01 +1100

    Decorate controllers and removed unused ones

--- api/app/controllers/mno_enterprise/jpi/v1/app_instances_controller.rb
+++ api/app/controllers/mno_enterprise/jpi/v1/app_instances_controller.rb
@@ -1,33 +1,5 @@
 module MnoEnterprise
   class Jpi::V1::AppInstancesController < Jpi::V1::BaseResourceController
-
-    # GET /mnoe/jpi/v1/organization/1/apps.json?timestamp=151452452345
-    def index
-      @app_instances = parent_organization.app_instances.active.where("updated_at.gt" => Time.at(timestamp)).select do |i|
-        # force owner assignment to avoid a refetch in ability can?(:access,i)
-        i.owner = parent_organization
-        can?(:access,i)
-      end
-    end
-
-    # POST /mnoe/jpi/v1/organization/1/app_instances
-    def create
-      authorize! :manage_app_instances, parent_organization
-      app_instance = parent_organization.app_instances.create(product: params[:nid])
-      head :created
-    end
-
-    # DELETE /mnoe/jpi/v1/app_instances/1
-    def destroy
-      app_instance = MnoEnterprise::AppInstance.find(params[:id])
-
-      if app_instance
-        authorize! :manage_app_instances, app_instance.owner
-        MnoEnterprise::EventLogger.info('app_destroy', current_user.id, "App destroyed", app_instance.name,app_instance)
-        app_instance.terminate
-      end
-
-      head :accepted
-    end
+    include MnoEnterprise::Concerns::Controllers::Jpi::V1::AppInstancesController
   end
 end
--- api/app/controllers/mno_enterprise/jpi/v1/marketplace_controller.rb
+++ api/app/controllers/mno_enterprise/jpi/v1/marketplace_controller.rb
@@ -1,22 +1,5 @@
 module MnoEnterprise
   class Jpi::V1::MarketplaceController < ApplicationController
-    respond_to :json
-
-    # GET /mnoe/mnoe/jpi/v1/marketplace
-    def index
-      @apps = if MnoEnterprise.marketplace_listing
-        MnoEnterprise::App.where('nid.in' => MnoEnterprise.marketplace_listing).to_a
-      else
-        MnoEnterprise::App.all.to_a
-      end
-      @apps.sort_by! { |app| [app.rank ? 0 : 1 , app.rank] } # the nil ranks will appear at the end
-      @categories = MnoEnterprise::App.categories(@apps)
-      @categories.delete('Most Popular')
-    end
-
-    # GET /mnoe/jpi/v1/marketplace/1
-    def show
-      @app = MnoEnterprise::App.find(params[:id])
-    end
+    include MnoEnterprise::Concerns::Controllers::Jpi::V1::MarketplaceController
   end
 end
