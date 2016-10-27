e058684c685a2d8d4f04c8d1f5e943c4a822b22a
Clement <clementberti@yahoo.fr>
2016-10-27 18:01:50 +1100

    Added another error status to the handler

--- core/app/controllers/mno_enterprise/application_controller.rb
+++ core/app/controllers/mno_enterprise/application_controller.rb
@@ -19,13 +19,20 @@ module MnoEnterprise
       include MnoEnterprise::Concerns::Controllers::AngularCSRF
     end
 
-    rescue_from Faraday::ConnectionFailed do |_e|
+    rescue_from Faraday::ConnectionFailed, Faraday::TimeoutError do |e|
+      case e
+        when Faraday::ConnectionFailed
+          status = :service_unavailable
+        when Faraday::TimeoutError
+          status = :too_many_requests
+      end
+
       respond_to do |format|
         format.html do
           @meta = {title: 'Backend unavailable'}
-          render 'error_page', layout: 'mno_enterprise/public', status: :service_unavailable
+          render 'error_page', layout: 'mno_enterprise/public', status: status
         end
-        format.json { render json: {error: 'API hub unavailable'}, status: :service_unavailable }
+        format.json { render json: {error: 'API hub unavailable'}, status: status }
       end
     end
 
