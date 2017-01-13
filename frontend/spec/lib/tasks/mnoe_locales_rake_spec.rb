4ac6dc9cd541510c0c2e23a1f158b63bdfc6cf0e
Clement <clementberti@yahoo.fr>
2017-01-13 16:57:30 +1100

    Added auto locales generation on build

--- frontend/spec/lib/tasks/mnoe_locales_rake_spec.rb
+++ frontend/spec/lib/tasks/mnoe_locales_rake_spec.rb
@@ -9,17 +9,23 @@ describe 'mnoe:locales:generate' do
   before do
     allow(MnoEnterprise::Frontend::LocalesGenerator).to receive(:new) { generator }
     allow(generator).to receive(:generate_json)
+    allow_any_instance_of(Rake::FileUtilsExt).to receive(:cp_r)
   end
 
   it { expect(subject.prerequisites).to include('environment') }
 
   it 'creates a generator with the correct folder' do
     subject.invoke
-    expect(MnoEnterprise::Frontend::LocalesGenerator).to have_received(:new).with('public/dashboard/locales')
+    expect(MnoEnterprise::Frontend::LocalesGenerator).to have_received(:new).with('tmp/build/frontend/src/locales')
   end
 
   it 'generates the locales' do
     subject.invoke
     expect(generator).to have_received(:generate_json)
+  end
+
+  it 'copies the locales to the public folder' do
+    expect_any_instance_of(Rake::FileUtilsExt).to receive(:cp_r).with('tmp/build/frontend/src/locales/', 'public/dashboard/locales/')
+    subject.invoke
   end
 end
