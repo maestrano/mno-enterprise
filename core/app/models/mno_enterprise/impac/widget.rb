84167026e583606cff70e4f0ae75ef6dade9fc6b
Cesar Tonnoir <cesar.tonnoir@gmail.com>
2017-02-01 19:29:17 +1100

    rename widget_category to endpoint (FE interface)

--- core/app/models/mno_enterprise/impac/widget.rb
+++ core/app/models/mno_enterprise/impac/widget.rb
@@ -1,15 +1,15 @@
 module MnoEnterprise
   class Impac::Widget < BaseResource
 
+    # TODO: change :widget_category to :endpoint when mnohub migrated to new model
     attributes :name, :width, :widget_category, :settings
-
     alias_attribute :endpoint, :widget_category
 
     belongs_to :dashboard, class_name: 'MnoEnterprise::Impac::Dashboard'
     has_many :kpis, class_name: 'MnoEnterprise::Impac::Kpi'
 
     def to_audit_event
-      {name: name}
+      { name: name }
     end
 
   end
