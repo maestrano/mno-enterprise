1268d623594853ce07e4176e344e261ef69cd8fc
hedudelgado <hedu.delgado@gmail.com>
2017-05-31 14:14:38 +0100

    [MNO-643] Improve working

--- core/app/helpers/mno_enterprise/image_helper.rb
+++ core/app/helpers/mno_enterprise/image_helper.rb
@@ -6,13 +6,13 @@ module MnoEnterprise
     # Helper method to easily access and select the images
     # If full_path is true returns filename appended to the path
     # If full_path is false returns filename
+    # Return the main-logo-whitebg.png if exists, otherwise get main-logo.png
     def main_logo_white_bg_path(full_path=false)
-      # Return the main-logo-whitebg.png if exists, otherwise get main-logo.png
       logo_path = app_image_path("main-logo-whitebg.png") || main_logo_path
       full_path ? logo_path : "mno_enterprise/#{File.basename(logo_path)}"
     end
 
-    # Return the main-logo.png if exists otherwise get for framework logo
+    # Return the main-logo.png if exists otherwise get engine's logo
     def main_logo_path
       file_name = "main-logo.png"
       app_image_path(file_name) || engine_image_path(file_name)
@@ -24,7 +24,7 @@ module MnoEnterprise
       app_path if File.exists?(app_path)
     end
 
-    # Get framework logo and return it
+    # Get engine's logo and return it
     def engine_image_path(file_name)
       "#{MnoEnterprise::Engine.root}#{IMAGES_LOCATION}#{file_name}"
     end
