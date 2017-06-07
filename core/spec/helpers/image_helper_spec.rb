23bf3609944fb5cbce3596d575f1f1da81ede3df
hedudelgado <hedu.delgado@gmail.com>
2017-06-07 18:11:30 +0100

    [MNOE-302] Refactor specs

--- core/spec/helpers/image_helper_spec.rb
+++ core/spec/helpers/image_helper_spec.rb
@@ -2,41 +2,67 @@ require 'rails_helper'
 
 module MnoEnterprise
   describe ImageHelper do
-    
-    IMAGES_LOCATION = "/app/assets/images/mno_enterprise"
+
+    let(:folder) { "/app/assets/images/mno_enterprise" }
+    let(:root) {"#{Rails.root}#{folder}"}
+    let(:path_engine_main_logo) { "#{MnoEnterprise::Engine.root}#{folder}/main-logo.png" }    
+    let(:path_main_logo) { "#{root}/main-logo.png" }
+    let(:path_main_logo_white) { "#{root}/main-logo-whitebg.png" }
 
     describe "#main_logo_white_bg_path" do
-      it "should return the engine's main-logo filename, when there are no logos"  do
-        expect(helper.main_logo_white_bg_path).to eq("mno_enterprise/main-logo.png")
-      end
-      
-      it "should return the engine's main-logo full path when there are no logos"  do
-        path = "#{MnoEnterprise::Engine.root}#{IMAGES_LOCATION}/main-logo.png"
-        expect(helper.main_logo_white_bg_path(true)).to eq(path)
-      end
-      
-      it "should return the main-logo-whitebg's filename when the file exists"  do
-        allow(File).to receive(:exists?).and_return(true)
-        expect(helper.main_logo_white_bg_path).to eq("mno_enterprise/main-logo-whitebg.png")
+    
+      context "when no logos exist"  do
+
+        it "returns the engine main-logo filename" do
+          expect(helper.main_logo_white_bg_path).to match("mno_enterprise/main-logo.png")
+        end
+
+        it "returns the engine main-logo full path"  do
+          expect(helper.main_logo_white_bg_path(true)).to match(path_engine_main_logo)
+        end
       end
 
-      it "should return the main-logo-whitebg's full path when the file exists"  do
-        path = "#{Rails.root}#{IMAGES_LOCATION}/main-logo-whitebg.png"
-        allow(File).to receive(:exists?).and_return(true)
-        expect(helper.main_logo_white_bg_path(true)).to eq(path)
+      context "when main-logo.png exists and main-logo-whitebg.png do not exist"  do
+        before { allow(File).to receive(:exists?).with(path_main_logo_white).and_return(false) }
+        before { allow(File).to receive(:exists?).with(path_main_logo).and_return(true) }
+        
+        it "returns the main-logo filename" do
+          expect(helper.main_logo_white_bg_path).to match("mno_enterprise/main-logo.png")
+        end
+
+        it "returns the main-logo full path"  do
+          expect(helper.main_logo_white_bg_path(true)).to match(path_main_logo)
+        end
+      end
+
+      context "when main-logo-whitebg.png exists"  do
+        before { allow(File).to receive(:exists?).with(path_main_logo_white).and_return(true) }
+        
+        it "returns the filename" do
+          expect(helper.main_logo_white_bg_path).to match("mno_enterprise/main-logo-whitebg.png")
+        end
+
+        it "returns the full path"  do
+          expect(helper.main_logo_white_bg_path(true)).to match(path_main_logo_white)
+        end
       end
     end
-
+    
     describe "#main_logo_path" do
-      it "should return the main-logo's path when main-logo exists"  do
-        path = "#{Rails.root}#{IMAGES_LOCATION}/main-logo.png"
-        allow(File).to receive(:exists?).and_return(true)
-        expect(helper.main_logo_path).to eq(path)
+
+      context "when there are no logos" do
+
+        it "returns the engines main-logo " do
+          expect(helper.main_logo_path).to match(path_engine_main_logo)
+        end
       end
-      
-      it "should return the engine's main-logo path when there are no logos"  do
-        path = "#{MnoEnterprise::Engine.root}#{IMAGES_LOCATION}/main-logo.png"
-        expect(helper.main_logo_path).to eq(path)
+
+      context "when main-logo.png exists"  do
+        before { allow(File).to receive(:exists?).with(path_main_logo).and_return(true) }
+
+        it "returns the full path"  do
+          expect(helper.main_logo_path).to match(path_main_logo)
+        end
       end
     end
   end
