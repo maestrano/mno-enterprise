fb5809d44e1a32f40f6040890268629fed77b8db
BrunoChauvet <bruno.chauvet@maestrano.com>
2017-06-28 18:45:53 +1000

    add routing spec

--- api/spec/routing/mno_enterprise/jpi/v1/subscriptions_controller_routing_spec.rb
+++ api/spec/routing/mno_enterprise/jpi/v1/subscriptions_controller_routing_spec.rb
@@ -15,5 +15,9 @@ module MnoEnterprise
     it 'routes to #create' do
       expect(post('/jpi/v1/organizations/1/subscriptions')).to route_to("mno_enterprise/jpi/v1/subscriptions#create", organization_id: '1')
     end
+
+    it 'routes to #update' do
+      expect(put('/jpi/v1/organizations/1/subscriptions/abc')).to route_to("mno_enterprise/jpi/v1/subscriptions#update", id: 'abc', organization_id: '1')
+    end
   end
 end
