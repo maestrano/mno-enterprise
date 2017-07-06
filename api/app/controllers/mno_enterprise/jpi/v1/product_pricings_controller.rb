bc1105f43ca6fb825f002ff7bfdc3306be0ab7e0
BrunoChauvet <bruno.chauvet@maestrano.com>
2017-07-06 11:50:39 +1000

    delete unused controller

--- api/app/controllers/mno_enterprise/jpi/v1/product_pricings_controller.rb
+++
@@ -1,5 +0,0 @@
-module MnoEnterprise
-  class Jpi::V1::ProductPricingsController < Jpi::V1::BaseResourceController
-    include MnoEnterprise::Concerns::Controllers::Jpi::V1::ProductPricingsController
-  end
-end
