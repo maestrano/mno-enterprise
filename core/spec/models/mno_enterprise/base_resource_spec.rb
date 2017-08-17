9df7e09b8b1200df52c9e878133c66e0f9a690af
Olivier Brisse <ouranos@users.noreply.github.com>
2017-08-17 15:06:43 +1000

    Pass locale to MnoHub

--- core/spec/models/mno_enterprise/base_resource_spec.rb
+++ core/spec/models/mno_enterprise/base_resource_spec.rb
@@ -5,5 +5,31 @@ module MnoEnterprise
 
     pending "add specs for MnoEnterprise::BaseResource: #{__FILE__}"
 
+    describe LocaleMiddleware do
+      before do
+        I18n.available_locales = [:'en-AU', :en]
+        I18n.locale = 'en-AU'
+      end
+
+      describe '.find' do
+        subject { BaseResource.find(1) }
+        let!(:stub) { stub_api_v2(:get, '/base_resources/1', nil, [], _locale: 'en-AU') }
+
+        it 'adds the locale to the query string' do
+          subject
+          expect(stub).to have_been_requested
+        end
+      end
+
+      describe '.where' do
+        subject { BaseResource.where(foo: 'bar').all }
+        let!(:stub) { stub_api_v2(:get, '/base_resources', nil, [], _locale: 'en-AU', filter: {foo: 'bar'}) }
+
+        it 'adds the locale to the query string' do
+          subject
+          expect(stub).to have_been_requested
+        end
+      end
+    end
   end
 end
