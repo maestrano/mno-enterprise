fe2fe4524486fc55f15f12d64eaed2e1d5f7e27f
Alexandre Jarnoux <alex.jarnoux@gmail.com>
2017-09-01 15:22:01 +1000

    [MNOE-492] Add assets in product view

--- api/app/controllers/mno_enterprise/jpi/v1/admin/assets_controller.rb
+++ api/app/controllers/mno_enterprise/jpi/v1/admin/assets_controller.rb
@@ -32,7 +32,7 @@ module MnoEnterprise
       product_id = params[:product_id] || params.dig(:asset,:product_id)
 
       # Retrieve the file temp path
-      content = params.dig(:asset,:content)
+      content = params[:content] || params.dig(:asset,:content)
       image_temp_path = content.tempfile.path
 
       # Read the file and encode content using base64
@@ -50,13 +50,13 @@ module MnoEnterprise
       asset.relationships.attributes = { product: { data: { type: 'products', id: product_id } } }
 
       if asset.save
-        head :created
+        render json: { id: asset.id, url: asset.url, field_name: asset.field_name }
       else
         render json: asset.errors, status: :bad_request
       end
     end
 
-    # DELETE /mnoe/jpi/v1/admin/products/1
+    # DELETE /mnoe/jpi/v1/admin/assets/1
     def destroy
       asset = MnoEnterprise::Asset.find_one(params[:id])
       return render_not_found('asset') unless asset
--- api/app/views/mno_enterprise/jpi/v1/admin/products/_product.json.jbuilder
+++ api/app/views/mno_enterprise/jpi/v1/admin/products/_product.json.jbuilder
@@ -7,6 +7,12 @@ json.values_attributes do
   end
 end
 
+json.assets_attributes do
+  json.array! product.assets.each do |asset|
+    json.extract! asset, :id, :url, :field_name
+  end
+end
+
 json.product_pricings do
   json.array! product.product_pricings.each do |pricing|
     json.extract! pricing, :id, :name, :description, :position, :free, :free_trial_enabled, :free_trial_duration, :free_trial_unit, :per_duration, :per_unit, :prices, :external_id
