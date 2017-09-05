8d8cba2aedf8e63af8af495a9f1a0ab391276d2e
Alexandre Jarnoux <alex.jarnoux@gmail.com>
2017-09-05 17:26:10 +1000

    [MNOE-492][MNOE-577] Fix Product routing spec

--- api/spec/routing/mno_enterprise/jpi/v1/admin/products_controller_routing_spec.rb
+++ api/spec/routing/mno_enterprise/jpi/v1/admin/products_controller_routing_spec.rb
@@ -4,34 +4,12 @@ module MnoEnterprise
   RSpec.describe Jpi::V1::Admin::ProductsController, type: :routing do
     routes { MnoEnterprise::Engine.routes }
 
-    context 'Local products are enabled' do
-      before(:all) do
-        Settings.merge!(dashboard: {marketplace: {local_products: true}})
-        Rails.application.reload_routes!
-      end
-
-      it 'routes to #index' do
-        expect(get('/jpi/v1/admin/products')).to route_to('mno_enterprise/jpi/v1/admin/products#index', format: 'json')
-      end
-
-      it 'routes to #show' do
-        expect(get('/jpi/v1/admin/products/1')).to route_to('mno_enterprise/jpi/v1/admin/products#show', id: '1', format: 'json')
-      end
+    it 'routes to #index' do
+      expect(get('/jpi/v1/admin/products')).to route_to('mno_enterprise/jpi/v1/admin/products#index', format: 'json')
     end
 
-    context 'Local products are disabled' do
-      before(:all) do
-        Settings.merge!(dashboard: {marketplace: {local_products: false}})
-        Rails.application.reload_routes!
-      end
-
-      it 'routes to #index' do
-        expect(get('/jpi/v1/admin/products')).not_to be_routable
-      end
-
-      it 'routes to #show' do
-        expect(get('/jpi/v1/admin/products/1')).not_to be_routable
-      end
+    it 'routes to #show' do
+      expect(get('/jpi/v1/admin/products/1')).to route_to('mno_enterprise/jpi/v1/admin/products#show', id: '1', format: 'json')
     end
   end
 end
