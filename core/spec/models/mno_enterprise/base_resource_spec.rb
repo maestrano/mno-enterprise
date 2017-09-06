f55773983848cec7eb42576b2df9b749954b799d
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2017-09-06 10:40:25 +1000

    fix time parsing

--- core/spec/models/mno_enterprise/base_resource_spec.rb
+++ core/spec/models/mno_enterprise/base_resource_spec.rb
@@ -5,6 +5,32 @@ module MnoEnterprise
 
     pending "add specs for MnoEnterprise::BaseResource: #{__FILE__}"
 
+    describe JsonApiClientExtension::CustomParser do
+      describe 'time parsing' do
+        subject { BaseResource.find(1).first }
+
+        let(:time_value) { '2017-09-06T00:16:22Z' }
+        let(:resp) { { data: { id: 1, type: 'base_resources', attributes: { some_date: time_value } } } }
+
+        before do
+          stub_request(:get, "#{MnoEnterprise::BaseResource.site}/base_resources/1?_locale=en").
+            to_return(status: 200, body: resp.to_json, headers: { content_type: 'application/vnd.api+json' })
+        end
+
+        describe 'with iso8601 date' do
+          it { expect { subject }.not_to raise_error }
+          it { expect(subject.some_date).to be_a(Time) }
+        end
+
+        describe 'with timezone expanded' do
+          let(:time_value) { '2017-09-06T00:16:22+0000' }
+
+          it { expect { subject }.not_to raise_error }
+          it { expect(subject.some_date).to be_a(Time) }
+        end
+      end
+    end
+
     describe LocaleMiddleware do
       before do
         I18n.available_locales = [:'en-AU', :en]
