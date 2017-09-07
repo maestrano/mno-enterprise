c7ffe280b4e7088b8f66a1dd33ab1c4f5cc6be50
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2017-09-07 22:45:18 +1000

    extend data model

--- core/app/models/mno_enterprise/bill.rb
+++ core/app/models/mno_enterprise/bill.rb
@@ -1,4 +1,12 @@
 module MnoEnterprise
   class Bill < BaseResource
+    property :created_at, type: :time
+    property :updated_at, type: :time
+    property :description, type: :string
+    property :price_cents, type: :integer
+    property :currency, type: :string
+
+    has_one :billable
+    has_one :invoice
   end
 end
