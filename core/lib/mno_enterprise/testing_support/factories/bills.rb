5e3a1de66bcb96665e2a569a2557e151351af82d
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2017-09-07 22:44:17 +1000

    add bill factory

---
+++ core/lib/mno_enterprise/testing_support/factories/bills.rb
@@ -0,0 +1,15 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :mno_enterprise_bill, class: 'Bill' do
+    factory :bill, class: MnoEnterprise::Bill do
+      sequence(:id)
+      created_at 3.days.ago
+      updated_at 1.hour.ago
+      price_cents 2000
+      end_user_price_cents 2400
+      currency 'AUD'
+      billable { build(:app_instance) }
+    end
+  end
+end
