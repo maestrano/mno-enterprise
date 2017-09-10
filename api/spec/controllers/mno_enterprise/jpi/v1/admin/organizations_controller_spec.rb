38d54fd0076748e4db70fb997dd646c8bf3e9388
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2017-09-10 12:32:08 +1000

    add sub-tenant scoping on organizations

--- api/spec/controllers/mno_enterprise/jpi/v1/admin/organizations_controller_spec.rb
+++ api/spec/controllers/mno_enterprise/jpi/v1/admin/organizations_controller_spec.rb
@@ -2,198 +2,135 @@ require 'rails_helper'
 
 module MnoEnterprise
   describe Jpi::V1::Admin::OrganizationsController, type: :controller do
-    include MnoEnterprise::TestingSupport::OrganizationsSharedHelpers
     include MnoEnterprise::TestingSupport::SharedExamples::JpiV1Admin
-    #TODO: Fix Spec for Admin Controller
-    before { skip }
 
     render_views
     routes { MnoEnterprise::Engine.routes }
     before { request.env["HTTP_ACCEPT"] = 'application/json' }
 
-    def hash_for_arrears(arrears)
-      {
-          'in_arrears' => arrears.map { |a| partial_hash_for_arrears(a) }
-      }
-    end
-
-    def partial_hash_for_arrears(arrear)
-      {
-          'name' => arrear.name,
-          'amount' => AccountingjsSerializer.serialize(arrear.payment),
-          'category' => arrear.category,
-          'status' => arrear.status
-      }
-    end
-
     #===============================================
     # Assignments
     #===============================================
+    let(:user) { build(:user, :admin) }
+    let!(:current_user_stub) { stub_api_v2(:get, "/users/#{user.id}", user, %i(deletion_requests organizations orga_relations dashboards)) }
 
-    # Stub user and user call
-    let(:user) { build(:user, admin_role: 'admin') }
-    before do
-      api_stub_for(get: "/users/#{user.id}", response: from_api(user))
-      sign_in user
-    end
-
-
-    # Stub organization + associations
-    let!(:invoice) { build(:invoice, organization_id: organization.id) }
     let(:organization) { build(:organization) }
-    let(:arrears) { build(:arrears_situation) }
-    let(:org_invite) { build(:org_invite, organization: organization) }
+    let(:arrears_situation) { build(:arrears_situation) }
+    let(:app) { build(:app) }
     let(:app_instance) { build(:app_instance, organization: organization) }
-    let(:credit_card) { build(:credit_card, organization: organization) }
-
-    before do
-      organizations = [organization]
-      allow(organizations).to receive(:loaded?).and_return(true)
-      allow_any_instance_of(MnoEnterprise::User).to receive(:organizations).and_return(organizations)
-      api_stub_for(get: "/organizations/#{organization.id}/invoices", response: from_api([invoice]))
-      api_stub_for(get: "/organizations", response: from_api([organization]))
-      api_stub_for(get: "/organizations/#{organization.id}", response: from_api(organization))
-      api_stub_for(get: "/organizations/#{organization.id}/users", response: from_api([user]))
-      api_stub_for(get: "/organizations/#{organization.id}/org_invites", response: from_api([org_invite]))
-      api_stub_for(get: "/organizations/#{organization.id}/app_instances", response: from_api([app_instance]))
-      api_stub_for(get: "/organizations/#{organization.id}/credit_card", response: from_api([credit_card]))
-      api_stub_for(get: "/arrears_situations", response: from_api([arrears]))
-      api_stub_for(post: "/organizations", response: from_api([organization]))
-    end
-
-    let(:expected_hash_for_organizations) {
-      {
-        'organizations' => [{
-          'id' => organization.id,
-          'uid' => organization.uid,
-          'name' => organization.name,
-          'soa_enabled' => organization.soa_enabled,
-          'created_at' => organization.created_at,
-          'credit_card' => {'presence' => organization.credit_card?},
-          'account_frozen' => organization.account_frozen
-        }],
-        'metadata' => {'pagination' => {'count' => 1}}
-      }
-    }
 
     #===============================================
     # Specs
     #===============================================
-    describe '#index' do
+    before { sign_in user }
+
+    describe 'GET #index' do
       subject { get :index }
 
-      it_behaves_like 'a jpi v1 admin action'
-
-      context 'success' do
-        before { subject }
-
-        it { expect(response).to be_success }
-
-        it 'returns a list of organizations' do
-          expect(JSON.parse(response.body)).to eq(JSON.parse(expected_hash_for_organizations.to_json))
-        end
+      let(:data) { JSON.parse(response.body) }
+      let(:select_fields) do
+        {
+          organizations: [
+            :uid, :name, :account_frozen, :soa_enabled, :mails, :logo, :latitude, :longitude, :geo_country_code, :geo_state_code,
+            :geo_city, :geo_tz, :geo_currency, :metadata, :industry, :size, :financial_year_end_month, :credit_card, :financial_metrics, :created_at
+          ].join(',')
+        }
       end
+      let(:expected_params) { { fields: select_fields, _metadata: { act_as_manager: user.id } } }
+
+      before { stub_api_v2(:get, "/organizations", [organization], [], expected_params) }
+      before { subject }
+
+      it { expect(data['organizations'].first['id']).to eq(organization.id) }
     end
 
     describe 'GET #show' do
       subject { get :show, id: organization.id }
 
-      it_behaves_like 'a jpi v1 admin action'
+      let(:data) { JSON.parse(response.body) }
+      let(:includes) { [:app_instances, :'app_instances.app', :users, :'users.user_access_requests', :orga_relations, :invoices, :credit_card, :orga_invites, :'orga_invites.user'] }
+      let(:expected_params) { { _metadata: { act_as_manager: user.id } } }
 
-      context 'success' do
-        before { subject }
+      before { allow(app_instance).to receive(:app).and_return(app) }
+      before { allow(organization).to receive(:app_instances).and_return([app_instance]) }
+      before { allow(organization).to receive(:invoices).and_return([]) }
+      before { stub_api_v2(:get, "/organizations/#{organization.id}", organization, includes, expected_params) }
+      before { subject }
 
-        it { expect(response).to be_success }
-
-        # TODO: admin and normal views are different we should test another way
-        xit 'returns a complete description of the organization' do
-          expect(JSON.parse(response.body)).to eq(JSON.parse(admin_hash_for_organization(organization).to_json))
-        end
-      end
+      it { expect(data['organization']['id']).to eq(organization.id) }
     end
 
     describe 'GET #in_arrears' do
       subject { get :in_arrears }
 
-      it_behaves_like 'a jpi v1 admin action'
+      let(:data) { JSON.parse(response.body) }
 
-      context 'success' do
-        before { subject }
+      before { stub_api_v2(:get, "/arrears_situations", [arrears_situation]) }
+      before { subject }
 
-        it 'returns arrears with the organization name' do
-          expect(response).to be_success
-          expect(JSON.parse(response.body)).to eq(JSON.parse(hash_for_arrears([arrears]).to_json))
-        end
-      end
+      it { expect(data['in_arrears'].first['name']).to eq(arrears_situation.name) }
     end
 
     describe 'POST #create' do
-      let(:params) { FactoryGirl.attributes_for(:organization) }
-      before { allow(MnoEnterprise::Organization).to receive(:create) { organization } }
-
       subject { post :create, organization: params }
+      let(:params) { attributes_for(:organization) }
 
-      it_behaves_like 'a jpi v1 admin action'
+      let(:data) { JSON.parse(response.body) }
+      let(:includes) { [:app_instances, :'app_instances.app', :users, :'users.user_access_requests', :orga_relations, :invoices, :credit_card, :orga_invites, :'orga_invites.user'] }
 
-      it 'creates the organization' do
-        expect(MnoEnterprise::Organization).to receive(:create).with(params.slice(:name)) { organization }
-        subject
+      before { allow(app_instance).to receive(:app).and_return(app) }
+      before { allow(organization).to receive(:app_instances).and_return([app_instance]) }
+      before { allow(organization).to receive(:invoices).and_return([]) }
+      before { stub_api_v2(:get, "/organizations/#{organization.id}", organization, includes) }
+      before { expect(MnoEnterprise::Organization).to receive(:create).with(params.slice(:name)).and_return(organization) }
+
+      describe 'creation' do
+        before { subject }
+        it { expect(data['organization']['id']).to eq(organization.id) }
       end
 
-      it 'provision the app instances' do
-        params[:app_nids] = ['xero', app_instance.app.nid]
+      describe 'app provisioning' do
+        let(:params) { attributes_for(:organization).merge(app_nids: ['xero', app_instance.app.nid]) }
 
-        # Track the API call
-        create = false
-        stub = -> { create = true; from_api(app_instance) }
-        api_stub_for(post: "/organizations/#{organization.id}/app_instances", response: stub)
-
-        subject
-
-        expect(create).to be true
+        before { expect_any_instance_of(Organization).to receive(:provision_app_instance) }
+        before { subject }
+        it { expect(data['organization']['id']).to eq(organization.id) }
       end
     end
 
     describe 'POST #invite_member' do
-      before do
-        # Track the api call
-        @api_call = false
-        stub = -> { @api_call = true; from_api(org_invite) }
-        api_stub_for(post: "/organizations/#{organization.id}/org_invites", response: stub)
-      end
-
-      let(:params) { FactoryGirl.attributes_for(:user) }
       subject { post :invite_member, id: organization.id, user: params }
 
+      let(:data) { JSON.parse(response.body) }
+
+      before { stub_api_v2(:get, "/organizations/#{organization.id}", organization, [:orga_relations], { _metadata: { act_as_manager: user.id } }) }
+
       context 'with existing user' do
-        before { allow(MnoEnterprise::User).to receive(:find_by) { user } }
+        let(:params) { attributes_for(:user) }
+        let(:invited_user) { build(:user) }
+        let(:orga_invite) { build(:orga_invite) }
 
-        it_behaves_like 'a jpi v1 admin action'
-
-        it 'creates an invite' do
-          subject
-          expect(@api_call).to be true
-        end
+        before { allow(orga_invite).to receive(:user).and_return(invited_user) }
+        before { stub_api_v2(:get, "/users", invited_user, [:orga_relations], { filter: { email: params[:email] }, page: { number: 1, size: 1 } }) }
+        before { stub_api_v2(:get, "/orga_invites/#{orga_invite.id}", orga_invite, [:user]) }
+        before { expect(MnoEnterprise::OrgaInvite).to receive(:create).and_return(orga_invite) }
+        before { subject }
+        it { expect(data['user']['id']).to eq(invited_user.id) }
       end
 
       context 'with new user' do
-        before { allow(MnoEnterprise::User).to receive(:find_by) { nil } }
+        let(:params) { attributes_for(:user) }
+        let(:invited_user) { build(:user) }
+        let(:orga_invite) { build(:orga_invite) }
 
-        # Directly stubbing the controller method as user creation is a PITA to stub
-        let(:new_user) { build(:user, params.slice(:email, :name, :surname, :phone)) }
-        before { allow(controller).to receive(:create_unconfirmed_user) { new_user } }
-
-        it_behaves_like 'a jpi v1 admin action'
-
-        it 'creates a user' do
-          expect(controller).to receive(:create_unconfirmed_user) { new_user }
-          subject
-        end
-
-        it 'creates an invite' do
-          subject
-          expect(@api_call).to be true
-        end
+        before { allow(invited_user).to receive(:confirmed?).and_return(false) }
+        before { allow(controller).to receive(:create_unconfirmed_user).and_return(invited_user) }
+        before { allow(orga_invite).to receive(:user).and_return(invited_user) }
+        before { stub_api_v2(:get, "/users", nil, [:orga_relations], { filter: { email: params[:email] }, page: { number: 1, size: 1 } }) }
+        before { stub_api_v2(:get, "/orga_invites/#{orga_invite.id}", orga_invite, [:user]) }
+        before { expect(MnoEnterprise::OrgaInvite).to receive(:create).and_return(orga_invite) }
+        before { subject }
+        it { expect(data['user']['id']).to eq(invited_user.id) }
       end
     end
   end
