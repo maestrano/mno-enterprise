c9a9f3cb8231eb2b53afde47990ad348b25911d6
Rémi Garde <remi.garde@free.fr>
2017-09-19 14:59:26 +1000

    [MNOE-616] Feat(jpi/user): Allow access to currency settings
    permit updates to the user settings default_widget_currency and currencies

--- api/lib/mno_enterprise/concerns/controllers/jpi/v1/current_users_controller.rb
+++ api/lib/mno_enterprise/concerns/controllers/jpi/v1/current_users_controller.rb
@@ -69,7 +69,7 @@ module MnoEnterprise::Concerns::Controllers::Jpi::V1::CurrentUsersController
     def user_params
       params.require(:user).permit(
         :name, :surname, :email, :company, :phone, :website, :phone_country_code, :current_password, :password, :password_confirmation,
-        settings: [:locale]
+        settings: [:locale, :default_widget_currency, currencies: []]
       )
     end
 
