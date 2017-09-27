8e6db038332123c5f8268f89330c0ecdd83024c6
Francois Gourichon <francois.gourichon@maestrano.com>
2017-09-27 16:36:58 +1000

    [MNOE-674] - Added spec

---
+++ core/spec/models/mno_enterprise/orga_invite_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+module MnoEnterprise
+  RSpec.describe OrgaInvite, type: :model do
+    describe '#accept!' do
+      let(:orga_invite) { FactoryGirl.build(:orga_invite) }
+      subject { orga_invite.accept! }
+
+      before { stub_api_v2(:patch, "/orga_invites/#{orga_invite.id}/accept", orga_invite) }
+
+      it 'accept invitations' do
+        subject
+        assert_requested_api_v2(:patch, "/orga_invites/#{orga_invite.id}/accept", body: {data: { attributes: { user_id: orga_invite.user.id } }}.to_json)
+      end
+    end
+
+    describe '#cancel!' do
+      let(:orga_invite) { FactoryGirl.build(:orga_invite) }
+      subject { orga_invite.cancel! }
+
+      before { stub_api_v2(:patch, "/orga_invites/#{orga_invite.id}/decline", orga_invite) }
+
+      it 'cancel invitation' do
+        subject
+        assert_requested_api_v2(:patch, "/orga_invites/#{orga_invite.id}/decline")
+      end
+    end
+
+    describe '#expired?' do
+      subject { orga_invite.expired? }
+
+      context 'invitation is cancelled' do
+        let(:orga_invite) { FactoryGirl.build(:orga_invite, status: 'cancelled') }
+
+        it { is_expected.to be true }
+      end
+
+      context 'invitation is 1 week old' do
+        let(:orga_invite) { FactoryGirl.build(:orga_invite, status: 'pending', created_at: 1.week.ago) }
+
+        it { is_expected.to be true }
+      end
+
+      context 'invitation is valid' do
+        let(:orga_invite) { FactoryGirl.build(:orga_invite, status: 'pending', created_at: 1.day.ago) }
+
+        it { is_expected.to be false }
+      end
+    end
+  end
+end
