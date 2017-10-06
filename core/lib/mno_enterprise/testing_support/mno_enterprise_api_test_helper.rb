4ad721b01e15efc6743b02c37639f5cd8704ede2
Olivier Brisse <ouranos@donarproject.org>
2017-10-06 13:59:00 +1100

    [MNOE-635] Remove Her test helpers

--- core/lib/mno_enterprise/testing_support/mno_enterprise_api_test_helper.rb
+++ core/lib/mno_enterprise/testing_support/mno_enterprise_api_test_helper.rb
@@ -1,11 +1,12 @@
 module MnoEnterpriseApiTestHelper
-
-  # Take a resource and transform it into a Hash describing
-  # the resource as if it had been returned by the MnoEnterprise
-  # API server
-  def from_api(res)
-    { data: serialize_type(res), metadata: {pagination: {count: entity_count(res)}} }
-  end
+  MOCK_OPTIONS = {
+    headers: {
+      'Accept' => 'application/vnd.api+json',
+      'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      'Content-Type' => 'application/vnd.api+json'
+    },
+    basic_auth: [MnoEnterprise.tenant_id, MnoEnterprise.tenant_key]
+  }
 
   def serialize_type(res)
     case
@@ -43,61 +44,6 @@ module MnoEnterpriseApiTestHelper
     end
   end
 
-  # Reset all API stubs.
-  # Called before each test (see spec_helper)
-  def api_stub_reset
-    ActiveSupport::Deprecation.warn('HER#api_stub_reset is now deprecated!', caller)
-    @_api_stub = nil
-    @_stub_list = {}
-    #
-    # api_stub_configure(Her::API.new)
-  end
-
-  # Example usage:
-  #
-  # Without opts, it yields a faraday stub object which you can configure
-  # manually:
-  #
-  # You can also pass the response stub via opts
-  # api_stub_for(User,
-  #   path: '/users/popular',
-  #   response: [{ id: 1, name: "Tobias Fünke" }, { id: 2, name: "Lindsay Fünke" }]
-  # )
-  #
-  # You can also specify the response code:
-  # api_stub_for(User,
-  #   path: '/users/popular',
-  #   code: 200,
-  #   response: [{ id: 1, name: "Tobias Fünke" }, { id: 2, name: "Lindsay Fünke" }]
-  # )
-  def api_stub_for(klass, opts = {})
-    real_opts = klass
-    if klass.is_a?(Class)
-      warn("DEPRECATION WARNING: api_stub_for(MyClass,{ some: 'opts'}) is deprecated. Please use api_stub_for({ some: 'opts' }) from now on")
-      real_opts = opts
-    end
-
-    set_api_stub
-    api_stub_add(real_opts)
-    api_stub_configure(@_api_stub)
-  end
-
-
-  # Remove an API stub added with `api_stub_for`
-  # This needs to be called with the same options
-  def remove_api_stub(opts = {})
-    set_api_stub
-    api_stub_remove(opts)
-    api_stub_configure(@_api_stub)
-  end
-
-  # Remove all api stubs
-  def clear_api_stubs
-    set_api_stub
-    @_stub_list = {}
-    api_stub_configure(@_api_stub)
-  end
-
   def stub_audit_events
     stub_api_v2(:post, '/audit_events')
   end
@@ -112,15 +58,6 @@ module MnoEnterpriseApiTestHelper
     url+="?#{params.to_query}" if params.any?
     url
   end
-
-  MOCK_OPTIONS = {
-    headers: {
-      'Accept' => 'application/vnd.api+json',
-      'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-      'Content-Type' => 'application/vnd.api+json'
-    },
-    basic_auth: [MnoEnterprise.tenant_id, MnoEnterprise.tenant_key]
-  }
 
   # Emulate the answer returned by the API V2. Returns a subset of json defined by the jsonapi-resources spec, so that it can be read by json api client
   def stub_api_v2(method, suffix, entity = nil, included = [], params = {})
@@ -158,122 +95,65 @@ module MnoEnterpriseApiTestHelper
 
   private
 
-    def type(entity)
-      entity.class.name.to_s.demodulize.underscore.pluralize
-    end
+  def type(entity)
+    entity.class.name.to_s.demodulize.underscore.pluralize
+  end
 
-    def entity_key(entity)
-      "#{type(entity)}/#{entity.id}"
-    end
+  def entity_key(entity)
+    "#{type(entity)}/#{entity.id}"
+  end
 
-    def serialize_relation(r, included_entities, inclusions = [])
-      included_entities[entity_key(r)] = [r, inclusions]
-      { type: type(r), id: r.id }
-    end
+  def serialize_relation(r, included_entities, inclusions = [])
+    included_entities[entity_key(r)] = [r, inclusions]
+    { type: type(r), id: r.id }
+  end
 
-    def serialize_data(entity, included, included_entities)
-      relationships = included.map do |field|
-        # Split multi-level fieds such as includes=app_instances.app
-        fields = field.to_s.split('.')
+  def serialize_data(entity, included, included_entities)
+    relationships = included.map do |field|
+      # Split multi-level fieds such as includes=app_instances.app
+      fields = field.to_s.split('.')
 
-        # Call first level of association
-        relations = entity.send(fields.shift)
-        next unless relations
+      # Call first level of association
+      relations = entity.send(fields.shift)
+      next unless relations
 
-        data = if relations.kind_of?(Array)
-                 relations.map { |r| serialize_relation(r, included_entities, fields) }
-               else
-                 serialize_relation(relations, included_entities, fields)
-               end
-        [field, { data: data }]
-      end.compact.to_h
-
-      {
-        id: entity.id,
-        type: type(entity),
-        attributes: serialize_type(entity),
-        relationships: relationships
-      }
-    end
-
-    def from_apiv2(entity, included)
-      included_entities = {}
-      data = if entity.kind_of?(Array)
-               entity.map{|e| serialize_data(e, included, included_entities)}
+      data = if relations.kind_of?(Array)
+               relations.map { |r| serialize_relation(r, included_entities, fields) }
              else
-               serialize_data(entity, included, included_entities)
+               serialize_relation(relations, included_entities, fields)
              end
+      [field, { data: data }]
+    end.compact.to_h
 
-      # Generate first and second level inclusions. E.g. includes='app_instances.app'
-      # The second level entities get added to the included_entities hash
-      included_entities.values.each { |obj, inclusions| serialize_data(obj, inclusions, included_entities) }
+    {
+      id: entity.id,
+      type: type(entity),
+      attributes: serialize_type(entity),
+      relationships: relationships
+    }
+  end
 
-      {
-        data: data,
-        meta: {
-          record_count: entity_count(entity)
-        },
-        included: included_entities.values.map { |obj, inclusions| serialize_data(obj, inclusions, included_entities) }
-      }
-    end
+  # Take a resource and transform it into a Hash describing
+  # the resource as if it had been returned by the MnoEnterprise
+  # API server
+  def from_apiv2(entity, included)
+    included_entities = {}
+    data = if entity.kind_of?(Array)
+             entity.map{|e| serialize_data(e, included, included_entities)}
+           else
+             serialize_data(entity, included, included_entities)
+           end
 
+    # Generate first and second level inclusions. E.g. includes='app_instances.app'
+    # The second level entities get added to the included_entities hash
+    included_entities.values.each { |obj, inclusions| serialize_data(obj, inclusions, included_entities) }
 
-    # Set a stub api on the provider class
-    def set_api_stub
-      ActiveSupport::Deprecation.warn('HER#set_api_stub is now deprecated!', caller)
-      nil
-      # return @_api_stub if @_api_stub
-      # @_api_stub = Her::API.new
-      # allow(MnoEnterprise::BaseResource).to receive(:her_api).and_return(@_api_stub = Her::API.new)
-      # @_api_stub
-    end
-
-    # Add a stub to the api
-    # E.g.:
-    # {
-    #   path: '/users/popular',
-    #   code: 200,
-    #   response: [{ id: 1, name: "Tobias Fünke" }, { id: 2, name: "Lindsay Fünke" }]
-    # }
-    def api_stub_add(orig_opts)
-      @_stub_list ||= {}
-      opts = orig_opts.dup
-
-      expand_options(opts)
-
-      key = opts.to_param
-      @_stub_list[key] = opts
-    end
-
-    # Remove an API
-    # This need to be called with the exact same options as `api_stub_add` was called with
-    def api_stub_remove(orig_opts)
-      @_stub_list ||= {}
-      opts = orig_opts.dup
-
-      expand_options(opts)
-
-      key = opts.to_param
-      @_stub_list.delete(key)
-    end
-
-    # Expand options so that: { put: '/path' } becomes { path: '/path', method: :put }
-    def expand_options(opts)
-      # Expand options so that: { put: '/path' } becomes { path: '/path', method: :put }
-      unless opts[:method] && opts[:path]
-        [:get, :put, :post, :delete].each do |verb|
-          if path = opts.delete(verb)
-            opts[:path] = path
-            opts[:method] = verb
-          end
-        end
-      end
-      key = opts.to_param
-      @_stub_list[key] = opts
-    end
-
-    # Configure the api and apply a list of stubs
-    def api_stub_configure(api)
-      ActiveSupport::Deprecation.warn('HER#api_stub_configure is now deprecated!', caller)
-    end
+    {
+      data: data,
+      meta: {
+        record_count: entity_count(entity)
+      },
+      included: included_entities.values.map { |obj, inclusions| serialize_data(obj, inclusions, included_entities) }
+    }
+  end
 end
