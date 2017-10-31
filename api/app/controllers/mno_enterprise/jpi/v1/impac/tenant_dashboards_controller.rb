3d6b5d4aa3add7e4bd55fedfc7ac5f102ddd7f8a
Xaun Lopez <xaunlopez@gmail.com>
2017-10-31 15:26:35 +0000

    Remove tenant_dashboards, add dashboard owner relationship

--- api/app/controllers/mno_enterprise/jpi/v1/impac/tenant_dashboards_controller.rb
+++
@@ -1,5 +0,0 @@
-module MnoEnterprise
-  class Jpi::V1::Impac::TenantDashboardsController < Jpi::V1::BaseResourceController
-    include MnoEnterprise::Concerns::Controllers::Jpi::V1::Impac::TenantDashboardsController
-  end
-end
