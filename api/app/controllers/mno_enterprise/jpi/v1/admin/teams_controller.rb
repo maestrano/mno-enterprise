b5adb293e87f9abe2c348f823077610ab17b34e5
hedudelgado <hedu.delgado@gmail.com>
2017-11-14 13:51:38 +0000

    [MNOE-146] Add teams admin controller

---
+++ api/app/controllers/mno_enterprise/jpi/v1/admin/teams_controller.rb
@@ -0,0 +1,11 @@
+module MnoEnterprise
+  class Jpi::V1::Admin::TeamsController < Jpi::V1::Admin::BaseResourceController
+
+    #==================================================================
+    # Instance methods
+    #==================================================================
+    # GET /mnoe/jpi/v1/admin/organizations/1/teams
+    def index
+    end
+  end
+end
