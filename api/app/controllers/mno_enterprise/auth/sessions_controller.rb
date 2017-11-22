21206e7042740dc746671229049f4b4ec89acf3b
Alexandre Jarnoux <alex.jarnoux@gmail.com>
2017-11-22 14:03:33 +1100

    Remove CSRF, not to be merged

--- api/app/controllers/mno_enterprise/auth/sessions_controller.rb
+++ api/app/controllers/mno_enterprise/auth/sessions_controller.rb
@@ -1,5 +1,8 @@
 module MnoEnterprise
   class Auth::SessionsController < Devise::SessionsController
     include MnoEnterprise::Concerns::Controllers::Auth::SessionsController
+
+    # TODO: warning: not to be merged, temp workaround
+    skip_before_filter :verify_authenticity_token
   end
 end
