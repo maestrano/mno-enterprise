9cc92c14beab44b209a71540fc6e1fc6a36f0c15
BrunoChauvet <bruno.chauvet@maestrano.com>
2017-11-29 16:54:09 +1100

    remove unused method + spec reorg

--- core/app/models/mno_enterprise/system_identity.rb
+++ core/app/models/mno_enterprise/system_identity.rb
@@ -3,12 +3,5 @@ module MnoEnterprise
     def self.table_name
       'system_identity'
     end
-
-    def to_audit_event
-      {
-        system_identity_id: id,
-        system_identity_name: name
-      }
-    end
   end
 end
--- core/spec/models/mno_enterprise/system_identity_spec.rb
+++ core/spec/models/mno_enterprise/system_identity_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 module MnoEnterprise
   RSpec.describe SystemIdentity, type: :model do
-    subject { described_class.table_name }
-
     describe '.table_name' do
+      subject { described_class.table_name }
+
       it { is_expected.to eql('system_identity') }
     end
   end
