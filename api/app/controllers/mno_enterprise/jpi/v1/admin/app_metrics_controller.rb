fa940db2973662898ac7f9ba529d060cb4d26858
Francois Gourichon <francois.gourichon@maestrano.com>
2017-12-06 15:57:05 +1100

    [MNOE-749] - Enable filter by terms on app_metrics

--- api/app/controllers/mno_enterprise/jpi/v1/admin/app_metrics_controller.rb
+++ api/app/controllers/mno_enterprise/jpi/v1/admin/app_metrics_controller.rb
@@ -3,10 +3,18 @@ module MnoEnterprise
 
     # GET /mnoe/jpi/v1/admin/app_metrics
     def index
+      if params[:terms]
+        # Search mode
+        @app_metrics = []
+
+        JSON.parse(params[:terms]).map { |t| @app_metrics = @app_metrics | MnoEnterprise::AppMetrics.where(Hash[*t]) }
+        response.headers['X-Total-Count'] = @app_metrics.count
+      else
         # Index mode
         query = MnoEnterprise::AppMetrics.apply_query_params(params)
         @app_metrics = query.to_a
         response.headers['X-Total-Count'] = query.meta.record_count
+      end
     end
 
     # GET /mnoe/jpi/v1/admin/app_metrics/1
