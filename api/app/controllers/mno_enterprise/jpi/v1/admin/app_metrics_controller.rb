64b0c21d514b5257bed80bd6f989336a8e74838e
MAhsenArif <ahsen.arif@square63.com>
2018-04-23 14:34:55 +0500

    [MNOE-992] Updated query conditions

--- api/app/controllers/mno_enterprise/jpi/v1/admin/app_metrics_controller.rb
+++ api/app/controllers/mno_enterprise/jpi/v1/admin/app_metrics_controller.rb
@@ -3,25 +3,27 @@ module MnoEnterprise
 
     # GET /mnoe/jpi/v1/admin/app_metrics
     def index
-      org_ids = current_user.client_ids if current_user.admin_role == 'staff'
+      staff_role = current_user.admin_role == 'staff'
+      org_ids = current_user.client_ids
+      @app_metrics = []
 
       if params[:terms]
         # Search mode
-        @app_metrics = []
-
-        if org_ids
-          JSON.parse(params[:terms]).map { |t| @app_metrics = @app_metrics | MnoEnterprise::AppMetrics.where(organization_ids: org_ids).where(Hash[*t]) }
-        else
+        unless staff_role
           JSON.parse(params[:terms]).map { |t| @app_metrics = @app_metrics | MnoEnterprise::AppMetrics.where(Hash[*t]) }
+        else
+          JSON.parse(params[:terms]).map { |t| @app_metrics = @app_metrics | MnoEnterprise::AppMetrics.where(organization_ids: org_ids).where(Hash[*t]) } if org_ids.present?
         end
-        response.headers['X-Total-Count'] = @app_metrics.count
+
       else
         # Index mode
-        query = MnoEnterprise::AppMetrics.apply_query_params(params)
-        query = query.where(organization_ids: org_ids ) if org_ids.present?
-        @app_metrics = query.to_a
-        response.headers['X-Total-Count'] = query.meta.record_count
+        if !staff_role || org_ids.present?
+          query = MnoEnterprise::AppMetrics.apply_query_params(params)
+          query = query.where(organization_ids: org_ids) if org_ids.present?
+          @app_metrics = query.to_a
+        end
       end
+      response.headers['X-Total-Count'] = @app_metrics.count
     end
 
     # GET /mnoe/jpi/v1/admin/app_metrics/1
