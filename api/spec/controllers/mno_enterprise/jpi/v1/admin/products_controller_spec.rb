b1d39274f471893c8b7a2b3525d383a978de29f3
Isaac Seessel <iseessel@oberlin.edu>
2018-04-29 15:38:46 -0400

    [OPAL-432] Add specs for #custom_schema endpoint

--- api/spec/controllers/mno_enterprise/jpi/v1/admin/products_controller_spec.rb
+++ api/spec/controllers/mno_enterprise/jpi/v1/admin/products_controller_spec.rb
@@ -28,9 +28,16 @@ module MnoEnterprise
     end
 
     describe 'GET #show' do
-      subject { get :show, id: product.id, editAction: 'SUSPEND' }
+      subject { get :show, id: product.id }
       let(:product) { build(:product) }
-      before { stub_api_v2(:get, "/products/#{product.id}", product, [:'values.field', :assets, :categories, :product_pricings, :product_contracts], {_edit_action: 'SUSPEND'}) }
+      before { stub_api_v2(:get, "/products/#{product.id}", product, [:'values.field', :assets, :categories, :product_pricings, :product_contracts]) }
+      it_behaves_like 'a jpi v1 admin action'
+    end
+
+    describe 'GET #custom_schema' do
+      subject { get :custom_schema, id: product.id, editAction: 'SUSPEND' }
+      let(:product) { build(:product) }
+      before { stub_api_v2(:get, "/products/#{product.id}", product, [], { _fetch_custom_schema: true, _edit_action: 'SUSPEND', fields: { products: 'custom_schema' } }) }
       it_behaves_like 'a jpi v1 admin action'
     end
   end
