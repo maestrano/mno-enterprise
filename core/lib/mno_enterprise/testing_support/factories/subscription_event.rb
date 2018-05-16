40b271b4467e393185e4f732fc2b5351a11679c4
Isaac Seessel <iseessel@oberlin.edu>
2018-05-16 13:24:39 -0400

    Fix specs

--- core/lib/mno_enterprise/testing_support/factories/subscription_event.rb
+++ core/lib/mno_enterprise/testing_support/factories/subscription_event.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :subscription_event, class: MnoEnterprise::SubscriptionEvent do
     sequence(:id, &:to_s)
-    
+    obsolete false
+
     subscription { build(:subscription) }
   end
 end
