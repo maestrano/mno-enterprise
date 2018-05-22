516248242c3d1bb159ae5678de5057153d33a9d9
Yuri Tomanek <yuri@tomanek.com.au>
2018-05-22 17:12:58 +0100

    [MNOE-1089] Subscriptions events - Capture and display the user
    Fixing issue with tests

--- core/lib/mno_enterprise/testing_support/factories/subscription_event.rb
+++ core/lib/mno_enterprise/testing_support/factories/subscription_event.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :subscription_event, class: MnoEnterprise::SubscriptionEvent do
     sequence(:id, &:to_s)
     obsolete false
+    user_name 'first last'
 
     subscription { build(:subscription) }
   end
