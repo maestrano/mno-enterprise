2a22e2639d5852e8491ebe4ca4803b743fe96de7
Olivier Brisse <ouranos@donarproject.org>
2018-06-03 18:16:50 +1000

    Re-add deleted specs
    This specs got deleted during the APIv2 migration

--- core/spec/models/mno_enterprise/base_resource_spec.rb
+++ core/spec/models/mno_enterprise/base_resource_spec.rb
@@ -23,6 +23,29 @@ module MnoEnterprise
       end
     end
 
+    describe '#cache_key' do
+      context 'for existing record' do
+        let(:user) { build(:user) }
+
+        it 'uses updated_at' do
+          expect(user.cache_key).to eq("mno_enterprise/users/#{user.id}-#{user.updated_at.utc.to_s(:nsec)}")
+        end
+
+        context 'when updated_at is nil' do
+          before { user.updated_at = nil }
+          it { expect(user.cache_key).to eq("mno_enterprise/users/#{user.id}") }
+        end
+
+        it 'uses the named timestamp' do
+          expect(user.cache_key(:confirmed_at)).to eq("mno_enterprise/users/#{user.id}-#{user.confirmed_at.utc.to_s(:nsec)}")
+        end
+      end
+
+      context 'for new record' do
+        it { expect(User.new.cache_key).to eq('mno_enterprise/users/new') }
+      end
+    end
+
     describe JsonApiClientExtension::CustomParser do
       describe 'time parsing' do
         subject { BaseResource.find(1).first }
