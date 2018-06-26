ee46964b4b18e66ed11c9c5b47b3c1f74f0b6822
paul <paulstuartparker@gmail.com>
2018-06-26 19:38:08 -0400

    [MNOE-1088][MNOE-1202] Spec Admin QuotesController

--- api/spec/routing/mno_enterprise/jpi/v1/admin/quotes_controller_routing_spec.rb
+++ api/spec/routing/mno_enterprise/jpi/v1/admin/quotes_controller_routing_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 module MnoEnterprise
-  RSpec.describe Jpi::V1::QuotesController, type: :routing do
+  RSpec.describe Jpi::V1::Admin::QuotesController, type: :routing do
     routes { MnoEnterprise::Engine.routes }
     context "Product provisioning is enabled" do
       before(:all) do
@@ -10,7 +10,7 @@ module MnoEnterprise
       end
 
       it 'routes to #create' do
-        expect(post('/jpi/v1/organizations/1/quotes')).to route_to("mno_enterprise/jpi/v1/quotes#create", organization_id: '1')
+        expect(post('/jpi/v1/admin/organizations/1/quotes')).to route_to("mno_enterprise/jpi/v1/admin/quotes#create", format: 'json', organization_id: '1')
       end
     end
 
@@ -21,7 +21,7 @@ module MnoEnterprise
       end
 
       it 'does not route to #create' do
-        expect(get('/jpi/v1/organizations/1/quotes')).not_to be_routable
+        expect(get('/jpi/v1/admin/organizations/1/quotes')).not_to be_routable
       end
     end
   end
