b70fcfe8603fa6f80cf2d8f40bc6de6348525405
Isaac Seessel <iseessel@oberlin.edu>
2018-06-28 18:37:25 -0400

    Dynamically configure routes based on admin panel support settings

--- api/spec/controllers/mno_enterprise/jpi/v1/admin/users_controller_spec.rb
+++ api/spec/controllers/mno_enterprise/jpi/v1/admin/users_controller_spec.rb
@@ -156,14 +156,12 @@ module MnoEnterprise
     describe 'POST #login_with_org_external_id' do
       subject { post :login_with_org_external_id, id: current_user.id, organization_external_id: organization_external_id }
 
-      before { Settings.merge!(admin_panel: {support: {enabled: enabled}}) }
-      let(:enabled) { true }
-      let(:organization_external_id) { 1 }
-
-      context 'with support settings disabled' do
-        let(:enabled) { false }
-        it { is_expected.not_to be_success }
+      before do
+        Settings.merge!(admin_panel: {support: {enabled: true}})
+        Rails.application.reload_routes!
       end
+
+      let(:organization_external_id) { 1 }
 
       context 'with support settings enabled' do
         let(:current_user) { build(:user, admin_role: admin_role) }
@@ -197,14 +195,11 @@ module MnoEnterprise
     describe 'DELETE #logout_support' do
       subject { delete :logout_support, { id: current_user.id }, { support_org_external_id: organization.external_id } }
 
-      before { Settings.merge!(admin_panel: {support: {enabled: enabled}}) }
-      let(:enabled) { true }
-      let(:organization) { build(:organization, external_id: 1) }
-
-      context 'with support settings disabled' do
-        let(:enabled) { false }
-        it { is_expected.not_to be_success }
+      before do
+        Settings.merge!(admin_panel: {support: {enabled: true}})
+        Rails.application.reload_routes!
       end
+      let(:organization) { build(:organization, external_id: 1) }
 
       context 'with support settings enabled' do
         let(:organization) { build(:organization) }
