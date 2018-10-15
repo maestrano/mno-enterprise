5f28a9db44ae0a2369c644fbcfdef5df3c472942
Isaac Seessel <iseessel@oberlin.edu>
2018-10-15 14:27:56 -0400

    Add route for admin#dashboards

---
+++ api/app/controllers/mno_enterprise/jpi/v1/admin/impac/dashboards_controller.rb
@@ -0,0 +1,31 @@
+module MnoEnterprise
+  class Jpi::V1::Admin::Impac::DashboardsController < Jpi::V1::Admin::BaseResourceController
+    skip_before_action :block_support_users, only: [:index]
+    before_filter :authorize_support, only: [:index]
+
+    # GET /mnoe/jpi/v1/admin/impac/dashboards
+    def index
+      # dashboards = MnoEnterprise::Dashboard
+      #           .apply_query_params(params.except(:terms))
+    end
+
+    private
+
+    def authorize_support
+      return unless current_user.support?
+
+      case params.dig('where', 'owner_type')
+      when 'User'
+        authorize!(:read, MnoEnterprise::User.new(id: params.dig('where', 'owner_id')))
+      when 'Organization'
+        authorize!(:read, MnoEnterprise::Organization.new(id: params.dig('where', 'owner_id')))
+      else
+        return head :forbidden
+      end
+    end
+
+    def valid_support_search?
+      params.dig('where', 'owner_id') && params.dig('where', 'owner_type') == 'User'
+    end
+  end
+end
