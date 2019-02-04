7974df9a43da7685a4bf18ebf68514b4521ed9b4
Cesar Tonnoir <cesar.tonnoir@gmail.com>
2019-02-04 11:36:58 +0000

    post-review changes on spec syntax

--- core/spec/lib/mno_enterprise/smtp_client_spec.rb
+++ core/spec/lib/mno_enterprise/smtp_client_spec.rb
@@ -2,82 +2,54 @@ require 'rails_helper'
 
 describe MnoEnterprise::SmtpClient do
   let(:client) { described_class.send(:new) }
-  let(:from_object) { { name: 'John', email: 'john.doe@example.com' } }
-  let(:to_object) { { name: 'Joe', email: 'joe.blogg@example.com' } }
-  let(:template_name) { 'reset-password-instructions' }
-  let(:expected_params) do
-    { from: 'John <john.doe@example.com>',
-      to: 'joe.blogg@example.com',
-      template_path: 'system_notifications',
-      template_name: template_name }
-  end
 
   describe '#deliver' do
-    context 'opts has been inputted by user' do
-      let(:opts) { {} }
-      subject do
-        client.deliver(
-          template_name,
-          from_object,
-          to_object,
-          { attachments: [{ name: 'some-file.pdf',
-                            content: 'the file' }] },
-          opts
-        )
-      end
-      context 'subject has been specified by user' do
-        let(:user_specified_subject) { 'This is a user specified subject' }
-
-        before do
-          opts[:subject] = user_specified_subject
-        end
-
-        it 'sets the subject to subject in opts' do
-          expected_params[:subject] = user_specified_subject
-
-          expect(client).to receive(:mail).with(expected_params)
-
-          subject
-        end
-      end
+    let(:opts) { nil }
+    let(:params) do
+      [
+        'reset-password-instructions',
+        { name: 'John', email: 'john.doe@example.com' },
+        { name: 'Joe', email: 'joe.blogg@example.com' },
+        { attachments: [{ name: 'some-file.pdf', content: 'the file' }] },
+        opts
+      ].compact
     end
+    let(:expected_params) do
+      {
+        from: 'John <john.doe@example.com>',
+        to: 'joe.blogg@example.com',
+        subject: client.humanize('reset-password-instructions'),
+        template_path: 'system_notifications',
+        template_name: 'reset-password-instructions'
+      }
+    end
+    
+    subject { client.deliver(*params) }
 
-    context 'opts has not been inputted by user' do
-      subject do
-        client.deliver(
-          template_name,
-          from_object,
-          to_object,
-          attachments: [{ name: 'some-file.pdf',
-                          content: 'the file' }]
-        )
-      end
-
-      before do
-        expected_params[:subject] = client.humanize(template_name)
-      end
-
-      it 'sets the subject of the email to the #humanize(template)' do
-        expect(client).to receive(:mail).with(expected_params)
-
-        subject
-      end
-
+    context 'with no subject specified' do
       it 'calls mail with the correct params' do
         expect(client).to receive(:mail).with(expected_params)
-
         subject
       end
 
       it 'sends the attachments' do
         allow(client).to receive(:mail)
-
         attachments = {}
         expect(client).to receive(:attachments).and_return(attachments)
-
         subject
-
         expect(attachments).to eq('some-file.pdf' => 'the file')
+      end
+    end
+    
+    context 'with a subject specified in the opts' do
+      let(:specified_subject) { 'This is a user specified subject' }
+      let(:opts) { { subject: specified_subject } }
+
+      before { expected_params[:subject] = specified_subject }
+
+      it 'uses the specified subject' do
+        expect(client).to receive(:mail).with(expected_params)
+        subject
       end
     end
   end
