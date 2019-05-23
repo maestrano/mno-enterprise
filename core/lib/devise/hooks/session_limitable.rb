bff511312de873eff21a71704fc17f2e75151d69
Steven Chang <steven.chang@maestrano.com>
2019-05-23 15:18:47 +1000

    Apply session_limitable guards in hooks

--- core/lib/devise/hooks/session_limitable.rb
+++ core/lib/devise/hooks/session_limitable.rb
@@ -2,7 +2,9 @@
 # This is only triggered when the user is explicitly set (with set_user)
 # and on authentication. Retrieving the user from session (:fetch) does
 # not trigger it.
-Warden::Manager.after_set_user :except => :fetch do |record, warden, options|
+Warden::Manager.after_set_user except: :fetch do |record, warden, options|
+  return unless Settings&.authentication&.session_limitable&.enabled
+
   if record.respond_to?(:update_unique_session_id!) && warden.authenticated?(options[:scope])
     unique_session_id = Devise.friendly_token
     warden.session(options[:scope])['unique_session_id'] = unique_session_id
@@ -13,7 +15,9 @@ end
 # Each time a record is fetched from session we check if a new session from another
 # browser was opened for the record or not, based on a unique session identifier.
 # If so, the old account is logged out and redirected to the sign in page on the next request.
-Warden::Manager.after_set_user :only => :fetch do |record, warden, options|
+Warden::Manager.after_set_user only: :fetch do |record, warden, options|
+  return unless Settings&.authentication&.session_limitable&.enabled
+
   scope = options[:scope]
   env   = warden.request.env
 
